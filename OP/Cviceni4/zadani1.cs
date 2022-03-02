5efd53adf16ec769b7c948dd67c8a12e23d3087b
ekral <ekral@utb.cz>
2022-03-02 13:11:56 +0100

    Create zadani1.cs

---
+++ OP/Cviceni4/zadani1.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace ConsoleApp3
+{
+    class Osoba
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+
+        public Osoba(string jmeno, string prijmeni)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+        }
+    }
+
+    // doplnte konstruktor pro Studenta a Zamestnance
+    class Student : Osoba
+    {
+        public string Skupina { get; set; }
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
