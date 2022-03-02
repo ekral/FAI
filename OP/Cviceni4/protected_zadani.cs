9b180a3a098895efab1b6a7cffac424800729a82
ekral <ekral@utb.cz>
2022-03-02 13:26:04 +0100

    Create protected_zadani.cs

---
+++ OP/Cviceni4/protected_zadani.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace ConsoleApp3
+{
+    class Osoba
+    {
+        // zvolte spravny modifikator pristupu tak, aby cisloUctu bylo pristupne v tride potomka, ale nebylo pristupne v klientskem kodu
+        public int cisloUctu;
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+
+        public Osoba(string jmeno, string prijmeni)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+        }
+    }
+
+    // doplnte konstruktor pro Studenta a Zamestnance
+    class Student : Osoba
+    {
+        public string Skupina { get; set; }
+
+        public Student(string jmeno, string prijmeni, string skupina) : base(jmeno, prijmeni)
+        {
+            Skupina = skupina;
+        }
+
+        public void Vypis()
+        {
+            Console.WriteLine($"{Jmeno} {Prijmeni} {cisloUctu} {Skupina}"); // pujde prelozit
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // klientsky kod
+            Student student1 = new Student("Anezka", "Lengalova", "AXP1");
+            Console.WriteLine(student1.cisloUctu); // nepujde prelozit
+        }
+    }
+}            
