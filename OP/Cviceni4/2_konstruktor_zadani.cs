83e097da51d67a9b4134243102bb5cab4bfa6789
ekral <ekral@utb.cz>
2022-03-03 11:50:26 +0100

    Update 2_konstruktor_zadani.cs

--- OP/Cviceni4/2_konstruktor_zadani.cs
+++ OP/Cviceni4/2_konstruktor_zadani.cs
@@ -1,31 +1,51 @@
 using System;
 
-namespace ConsoleApp3
+namespace Ukol3
 {
     class Osoba
     {
+        // pouzijte spravny modifikator pristupu, tak aby cislo uctu 
+        // nebylo pristupne v klientskem kodu
+        // ale byl pristupny v tride potomka
+
+        public int cisloUctu; // zde zmente modifikator pristupu
         public string Jmeno { get; set; }
         public string Prijmeni { get; set; }
 
-        public Osoba(string jmeno, string prijmeni)
+        public Osoba(string jmeno, string prijmeni, int cisloUctu)
         {
             Jmeno = jmeno;
             Prijmeni = prijmeni;
+            this.cisloUctu = cisloUctu;
         }
     }
 
-    // doplnte konstruktor pro Studenta a Zamestnance
+
     class Student : Osoba
     {
+        
         public string Skupina { get; set; }
 
+        public Student(string jmeno, string prijmeni, int cisloUctu, string skupina)
+            : base(jmeno, prijmeni, cisloUctu)
+        {
+            Skupina = skupina;
+        }
+
+        public void Vypis()
+        {
+            Console.WriteLine($"{Jmeno} {Prijmeni} {cisloUctu} {Skupina}"); // pujde prelozit
+        }
     }
-    
+
     class Program
     {
         static void Main(string[] args)
         {
-
+            // klientsky kod
+            Student student = new Student("Bohumil", "Vesely", 123, "AXP1");
+            student.Vypis();
+            student.cisloUctu = 1; // tento radek nepujde prelozit
         }
     }
 }
