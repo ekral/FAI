f70ec19e079c7aedc2b6109c32c0d2871015a684
ekral <ekral@utb.cz>
2022-03-03 11:53:10 +0100

    Update 3_protected_reseni.cs

--- OP/Cviceni4/3_protected_reseni.cs
+++ OP/Cviceni4/3_protected_reseni.cs
@@ -1,27 +1,33 @@
 using System;
 
-namespace ConsoleApp3
+namespace Ukol3
 {
     class Osoba
     {
-        // zvolte spravny modifikator pristupu tak, aby cisloUctu bylo pristupne v tride potomka, ale nebylo pristupne v klientskem kodu
-        protected int cisloUctu;
+        // pouzijte spravny modifikator pristupu, tak aby cislo uctu 
+        // nebylo pristupne v klientskem kodu
+        // ale byl pristupny v tride potomka
+
+        protected int cisloUctu; // zde zmente modifikator pristupu
         public string Jmeno { get; set; }
         public string Prijmeni { get; set; }
 
-        public Osoba(string jmeno, string prijmeni)
+        public Osoba(string jmeno, string prijmeni, int cisloUctu)
         {
             Jmeno = jmeno;
             Prijmeni = prijmeni;
+            this.cisloUctu = cisloUctu;
         }
     }
 
-    // doplnte konstruktor pro Studenta a Zamestnance
+
     class Student : Osoba
     {
+        
         public string Skupina { get; set; }
 
-        public Student(string jmeno, string prijmeni, string skupina) : base(jmeno, prijmeni)
+        public Student(string jmeno, string prijmeni, int cisloUctu, string skupina)
+            : base(jmeno, prijmeni, cisloUctu)
         {
             Skupina = skupina;
         }
@@ -31,14 +37,15 @@ namespace ConsoleApp3
             Console.WriteLine($"{Jmeno} {Prijmeni} {cisloUctu} {Skupina}"); // pujde prelozit
         }
     }
-    
+
     class Program
     {
         static void Main(string[] args)
         {
             // klientsky kod
-            Student student1 = new Student("Anezka", "Lengalova", "AXP1");
-            Console.WriteLine(student1.cisloUctu); // nepujde prelozit
+            Student student = new Student("Bohumil", "Vesely", 123, "AXP1");
+            student.Vypis();
+            student.cisloUctu = 1; // tento radek nepujde prelozit
         }
     }
 }
