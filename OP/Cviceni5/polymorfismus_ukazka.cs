a0031c4b34851efefa57444026fe7c42788d5e24
ekral <ekral@utb.cz>
2022-03-09 13:42:04 +0100

    Update polymorfismus_ukazka.cs

--- OP/Cviceni5/polymorfismus_ukazka.cs
+++ OP/Cviceni5/polymorfismus_ukazka.cs
@@ -20,6 +20,13 @@ namespace ConsoleApp4
         }
     }
 
+    class Krava : Zviratko
+    {
+        public override void Zvuk()
+        {
+            Console.WriteLine("Mua Mua");
+        }
+    }
     class Kocicka : Zviratko
     {
         public int ZaplneniZachodu { get; set; }
@@ -28,10 +35,36 @@ namespace ConsoleApp4
             Console.WriteLine("Mnau");
         }
     }
+
+    class Zoo
+    {
+        public List<Zviratko> Zviratka { get; private set; } 
+        public Zoo()
+        {
+            Zviratka = new List<Zviratko>();
+        }
+        public void Pridej(Zviratko zviratko)
+        {
+            Zviratka.Add(zviratko);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Zoo lesna = new Zoo();
+           
+            lesna.Pridej(new Krava() { Jmeno = "Milka" });
+            lesna.Pridej(new Pejsek() { Jmeno = "Rex" });
+            lesna.Pridej(new Kocicka() { Jmeno = "Cat woman" });
+
+            foreach (Zviratko z in lesna.Zviratka)
+            {
+                Console.WriteLine(z.Jmeno);
+                z.Zvuk();
+            }
+
             List<Zviratko> zviratka = new List<Zviratko>();
             zviratka.Add(new Pejsek() { Jmeno = "komisar Rex" });
             zviratka.Add(new Kocicka() { Jmeno = "Cat Woman" });
