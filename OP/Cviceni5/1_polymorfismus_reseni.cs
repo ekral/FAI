9998904c54b8419e85c0a72b68fe7b81443e5f3c
ekral <ekral@utb.cz>
2022-03-10 12:30:28 +0100

    Update 1_polymorfismus_reseni.cs

--- OP/Cviceni5/1_polymorfismus_reseni.cs
+++ OP/Cviceni5/1_polymorfismus_reseni.cs
@@ -2,6 +2,8 @@ using Spectre.Console;
 using System;
 using System.Collections.Generic;
 
+// pridat nuget Spectre Console
+
 namespace ConsoleApp12
 {
     class Zviratko
@@ -16,6 +18,7 @@ namespace ConsoleApp12
 
     class Pejsek : Zviratko
     {
+        internal const string Druh = "Pejsek";
 
         override public string Zvuk()
         {
@@ -30,11 +33,21 @@ namespace ConsoleApp12
             return "mek mek";
         }
     }
+    class Kravicka : Zviratko
+    {
+        override public string Zvuk()
+        {
+            return "buu buu";
+        }
+    }
 
 
     class Program
     {
-        // TODO pouzit enum pro druhy zviratek
+        // Ukol 1: Pridejte novy druh zviratka
+        // Ukol 2: Ukoncete nekonecny cyklus
+        // Ukol 3 pokrocily: doplnte vice Zoo a doplnte Unit testy
+        // TODO pouzit konstanty pro druhy
 
         static void Main(string[] args)
         {
@@ -46,33 +59,33 @@ namespace ConsoleApp12
                 string druh = AnsiConsole.Prompt(
                     new SelectionPrompt<string>()
                     .Title("Zvol [green]zviratko[/]:")
-                    .AddChoices(new[] { "Pejsek", "Kacenka" })
+                    .AddChoices(new[] { Pejsek.Druh, "Kacenka", "Kravicka" })
                 );
 
                 string name = AnsiConsole.Ask<string>("Zadej [green]jmeno[/]:");
 
                 Zviratko nove = druh switch
                 {
-                    "Pejsek" => new Pejsek() { Jmeno = name },
+                    Pejsek.Druh => new Pejsek() { Jmeno = name },
                     "Kacenka" => new Kacenka() { Jmeno = name },
+                    "Kravicka" => new Kravicka() { Jmeno = name },
                     _ => throw new ArgumentException("Neplatny druh zviratka")
                 };
 
                 zviratka.Add(nove);
 
+                foreach (Zviratko zviratko in zviratka)
+                {
+                    Console.WriteLine(zviratko.Jmeno);
+                    Console.WriteLine(zviratko.Zvuk());
+                }
+
                 if (!AnsiConsole.Confirm("Pridat nove zviratko?"))
                 {
-                    
+                    konec = true;
                 }
 
             } while (!konec);
-
-
-            foreach (Zviratko zviratko in zviratka)
-            {
-                Console.WriteLine(zviratko.Jmeno);
-                Console.WriteLine(zviratko.Zvuk());
-            }
         }
     }
 }
