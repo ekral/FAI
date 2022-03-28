8210ee027489661ddb3ecf7ab088970340ca6f3b
ekral <ekral@utb.cz>
2022-03-28 14:25:37 +0200

    Create 2_reseni.cs

---
+++ OP/Tema8/2_reseni.cs
@@ -0,0 +1,39 @@
+// Ukol 2: S pomocí Dictionary vytvořte a na konzoli zobrazte histogram vyskytu slov v libovolnem textovem souboru
+
+using Spectre.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string line = "Ahoj Ahoj Ahoj jak se jak se mas; mas,mas";
+        string[] slovaArray = line.Split(new char[] {',',' ', '.', ';' });
+
+        Dictionary<string, int> slova = new Dictionary<string, int>();
+
+        foreach (string slovo in slovaArray)
+        {
+            if (!string.IsNullOrWhiteSpace(slovo))
+            {
+                if (slova.ContainsKey(slovo))
+                {
+                    slova[slovo]++;
+                }
+                else
+                {
+                    slova[slovo] = 1;
+                }
+            }
+        }
+
+        Color[] palette = new Color[] { Color.Blue, Color.Blue1, Color.Blue3 };
+        int index = 0;
+
+        AnsiConsole.Write(new BarChart()
+            .Width(60)
+            .Label("[green bold underline]Vyskyt slov[/]")
+            .CenterLabel()
+            .AddItems(slova, (item) => new BarChartItem(
+                item.Key, item.Value, palette[index = ++index % palette.Count()])));
+    }
+}
