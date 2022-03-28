ec0f1f8a5c895ee019a2728a303dcc85aa378365
ekral <ekral@utb.cz>
2022-03-28 13:33:16 +0200

    Update 1_zadani.cs

--- OP/Tema8/1_zadani.cs
+++ OP/Tema8/1_zadani.cs
@@ -1,1 +1,36 @@
+// Ukol 1: Prepracujte tridu Sklad na generickou, tak aby byl validni klientsky kod v metode Main
 
+class Sklad
+{
+    int[] data;
+    private int pocet;
+
+    public Sklad(int kapacita)
+    {
+        data = new int[kapacita];
+    }
+
+    public void Zaloz(int objekt)
+    {
+        data[pocet++] = objekt;
+    }
+
+    public int Vyloz()
+    {
+        return data[--pocet];
+    }
+}
+
+class Program
+{
+        static void Main(string[] args)
+        {
+            Sklad<int>  skladInt = new Sklad<int>(10);
+            skladInt.Zaloz(1);
+            int celeCislo = skladInt.Vyloz();
+
+            Sklad<string> skladString = new Sklad<string>(10);
+            skladString.Zaloz("Ahoj");
+            string retezec = skladString.Vyloz();
+        }
+}
