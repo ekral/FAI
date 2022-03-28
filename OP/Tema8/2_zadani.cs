c42d60422095fe29d540de816212eedc8f2ebffe
ekral <ekral@utb.cz>
2022-03-28 13:37:15 +0200

    Create 2_zadani.cs

---
+++ OP/Tema8/2_zadani.cs
@@ -0,0 +1,12 @@
+// Ukol 1: S pomocí Dictionary vytvořte a na konzoli zobrazte histogram vyskytu slov v libovolnem textovem souboru
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string retezec = "Ahoj jak se mas";
+        string[] slova = retezec.Split(retezec);
+
+        // nebo muzu prochazet znaky postupne
+    }
+}
