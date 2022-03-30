c47de05746774314d29f1fdae0f0a75d7c55d786
ekral <ekral@utb.cz>
2022-03-30 13:47:20 +0200

    Update 2_reseni.cs

--- OP/Tema8/2_reseni.cs
+++ OP/Tema8/2_reseni.cs
@@ -1,40 +1,55 @@
-// Ukol 2: S pomocí Dictionary vytvořte a na konzoli zobrazte histogram vyskytu slov v libovolnem textovem souboru
+// Ukol 1: S pomocí Dictionary vytvořte a na konzoli zobrazte histogram vyskytu slov v libovolnem textovem souboru
 
 using Spectre.Console;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string line = "Ahoj Ahoj Ahoj jak se jak se mas; mas,mas";
-        string[] slovaArray = line.Split(new char[] { ',', ' ', '.', ';' });
+        // Domaci ukol: nacitat po jednom znaku
 
-        Dictionary<string, int> slova = new Dictionary<string, int>();
+        Dictionary<string, int> slovnik = new Dictionary<string, int>();
+        
+        using StreamReader reader = new StreamReader("text.txt");
+        
+        string line = null;
 
-        foreach (string slovo in slovaArray)
+        while ((line = reader.ReadLine()) != null)
         {
-            if (!string.IsNullOrWhiteSpace(slovo))
+            string[] slova = line.Split(new char[] { ',', ' ', '.', ';', '"', '[', ']', '„', '”', '?', '!' });
+
+            foreach (string slovo in slova)
             {
-                if (slova.ContainsKey(slovo))
+                if (!string.IsNullOrWhiteSpace(slovo))
                 {
-                    slova[slovo]++;
-                }
-                else
-                {
-                    slova[slovo] = 1;
+                    if (slovnik.ContainsKey(slovo))
+                    {
+                        slovnik[slovo]++;
+                    }
+                    else
+                    {
+                        slovnik[slovo] = 1;
+                    }
                 }
             }
         }
+
+        //foreach (var pair in slovnik.OrderBy(p => p.Value))
+        //{
+        //    System.Console.WriteLine($"{pair.Key}\t\t {pair.Value}");
+        //}
 
         Color[] palette = new Color[] { Color.Blue, Color.Blue1, Color.Blue3 };
         int index = 0;
 
         AnsiConsole.Write(new BarChart()
-            .Width(60)
-            .Label("[green bold underline]Vyskyt slov[/]")
-            .CenterLabel()
-            .AddItems(slova, (item) => new BarChartItem(
-                item.Key, item.Value, palette[index = ++index % palette.Length])));
+           .Width(60)
+           .Label("[green bold underline]Vyskyt slov[/]")
+           .CenterLabel()
+           .AddItems(slovnik.OrderByDescending(p => p.Value), (item) => new BarChartItem(
+               item.Key, item.Value, palette[index = ++index % palette.Length])));;
     }
 }
