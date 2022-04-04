7e9ac9293fa40037676a84aedcf42204be5fd4c6
ekral <ekral@utb.cz>
2022-04-04 12:15:35 +0200

    Create 01_vypis.cs

---
+++ OP/Tema9/01_vypis.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Test10
+{
+    delegate void MujDelegat(int x);
+
+    class Program
+    {
+        static void VypisA(int x)
+        {
+            Console.WriteLine($"A {x}");
+        }
+
+        static void VypisB(int x)
+        {
+            Console.WriteLine($"B {x}");
+        }
+
+        static void Main(string[] args)
+        {
+            MujDelegat d1 = VypisA;
+
+            d1?.Invoke(1); // vypise A 1
+
+            MujDelegat d2 = null; 
+
+            d2 += VypisA;
+            d2 += VypisB;
+
+            d2?.Invoke(2); // vypise A 2 a B 2
+
+            d2 -= VypisA;
+
+            d2?.Invoke(3); // vypise jen B 3
+        }
+    }
+}
