964696964c32d0953bbb41533f74771946f3e1ae
ekral <ekral@utb.cz>
2022-04-04 12:15:55 +0200

    Create 02_operace.cs

---
+++ OP/Tema9/02_operace.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Test10
+{
+    delegate int DelegatOperace(int x, int y);
+
+    class Program
+    {
+        static int Suma(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Soucin(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            DelegatOperace operace = Suma;
+            int vysledek = operace.Invoke(2, 3);
+            Console.WriteLine(vysledek);
+
+            operace = Soucin;
+            vysledek = operace.Invoke(2, 3);
+            Console.WriteLine(vysledek);
+        }
+    }
+}
