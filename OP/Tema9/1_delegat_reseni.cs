d1a58fcbc16c5dbb7a65c5bead9564d118cfbc80
ekral <ekral@utb.cz>
2022-04-05 12:47:24 +0200

    Update 1_delegat_reseni.cs

--- OP/Tema9/1_delegat_reseni.cs
+++ OP/Tema9/1_delegat_reseni.cs
@@ -1,9 +1,12 @@
+// Ukol Delegat: Definujte typ delegate pro metody Vypis a JeSude a vytvorte ukazku pouziti typu delegate
+
 using System;
 
 namespace ConsoleApp23
 {
     class Program
     {
+
         static int Soucet(int x, int y)
         {
             return x + y;
@@ -29,28 +32,23 @@ namespace ConsoleApp23
             return x > 0;
         }
 
-        delegate int Operace(int x, int y);
-
-        // Definujte typ delegate pro metodu Vypis a vytvorte ukazku pouziti typu delegate
-        delegate void VypisDelegat(string text);
-
-        // Definujte typ delegate pro metodu JeSude a vytvorte ukazku pouziti typu delegate
-        delegate bool SudeDelegat(int x);
-
+        delegate int MujDelegat1(int x, int y);
+        delegate void MulDelegat2(string text);
+        delegate bool MujDelegat3(int x);
         static void Main(string[] args)
         {
             int x = 2;
             int y = 3;
 
-            Operace o1 = Soucin;
-            int vysledek = o1.Invoke(x, y);
-            Console.WriteLine(vysledek);
+            MujDelegat1 d1 = Soucet;
+            int vysledek1 = d1.Invoke(x, y);
+            Console.WriteLine(vysledek1);
+            
+            MulDelegat2 d2 = Vypis;
+            d2.Invoke("ahoj");
 
-            VypisDelegat d1 = Vypis;
-            d1.Invoke("Ahoj");
-
-            SudeDelegat s1 = JeKladne;
-            bool vysledek2 = s1.Invoke(1);
+            MujDelegat3 d3 = JeSude;
+            bool vysledek2 = d3.Invoke(2);
             Console.WriteLine(vysledek2);
         }
     }
