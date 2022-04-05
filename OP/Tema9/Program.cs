1121b84088ef43dcc735c96ad0e5baf3deac80ec
ekral <ekral@utb.cz>
2022-04-05 10:41:59 +0200

    Add files via upload

---
+++ OP/Tema9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static int Soucet(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Soucin(int x, int y)
+        {
+            return x * y;
+        }
+
+        delegate int Operace(int x, int y);
+
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 3;
+            Operace o1 = Soucin;
+            int vysledek = o1.Invoke(x, y);
+            Console.WriteLine(vysledek);
+        }
+    }
+}
