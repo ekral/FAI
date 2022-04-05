884b4a78fe2e7a43c36be3b006c4fec0e1a9b33b
ekral <ekral@utb.cz>
2022-04-05 13:01:36 +0200

    Update 2_sablony_reseni.cs

--- OP/Tema9/2_sablony_reseni.cs
+++ OP/Tema9/2_sablony_reseni.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp23
 {
     class Program
     {
+
         static int Soucet(int x, int y)
         {
             return x + y;
@@ -32,30 +33,28 @@ namespace ConsoleApp23
             return x > 0;
         }
 
-        //delegate int Operace(int x, int y);
-
-        //// Definujte typ delegate pro metodu Vypis a vytvorte ukazku pouziti typu delegate
-        //delegate void VypisDelegat(string text);
-
-        //// Definujte typ delegate pro metodu JeSude a vytvorte ukazku pouziti typu delegate
-        //delegate bool SudeDelegat(int x);
+        //delegate int MujDelegat1(int x, int y);
+        //delegate void MulDelegat2(string text);
+        //delegate bool MujDelegat3(int x);
 
         static void Main(string[] args)
         {
-        
             int x = 2;
             int y = 3;
 
-            Func<int, int, int> o1 = Soucin;
-            int vysledek = o1.Invoke(x, y);
-            Console.WriteLine(vysledek);
+            Func<int, int, int> d1 = Soucet;
+            int vysledek1 = d1.Invoke(x, y);
+            Console.WriteLine(vysledek1);
+            
+            Action<string> d2 = Vypis; // Nahradte pomoci Action, Func nebo Predicate
+            d2.Invoke("ahoj");
 
-            Action<string> d1 = Vypis; // Pouzijte Action, Func nebo Predicate
-            d1.Invoke("Ahoj");
-
-            Predicate<int> s1 = JeKladne; // Pouzijte Action, Func nebo Predicate
-            bool vysledek2 = s1.Invoke(1);
+            Predicate<int> d3 = JeSude;  // Nahradte pomoci Action, Func nebo Predicate
+            bool vysledek2 = d3.Invoke(2);
             Console.WriteLine(vysledek2);
+
+            // Prepiste d3 s pouzitim Func
+            Func<int, bool> d4 = JeSude;
         }
     }
 }
