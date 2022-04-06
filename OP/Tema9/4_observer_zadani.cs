e63863444abbd4dd1f36f8b88fb3c6adee605b53
ekral <ekral@utb.cz>
2022-04-06 13:38:10 +0200

    Create 4_observer_zadani.cs

---
+++ OP/Tema9/4_observer_zadani.cs
@@ -0,0 +1,53 @@
+// Ukol 4 Observer: // Doplnte spravne klicove slovo, aby nasledujici radky nesly prelozit
+                   // a slo o navrhovy vzor observer
+
+using System;
+
+namespace Tema9
+{
+    class NakupniCentrum
+    {
+        public string Nazev { get; set; }
+        public Action<string> poplach;
+
+        public NakupniCentrum(string nazev)
+        {
+            Nazev = nazev;
+        }
+
+        public void Pozar()
+        {
+            poplach?.Invoke(Nazev);
+        }
+    }
+    class Program
+    {
+        public static void VyjezdHasicu(string nazev)
+        {
+            Console.WriteLine($"Jedeme hasit {nazev}");
+        }
+
+        public static void VyjezdPolicie(string nazev)
+        {
+            Console.WriteLine($"Jedeme delat pomahat a chranit do {nazev}");
+        }
+
+        static void Main(string[] args)
+        {
+            NakupniCentrum nakupniCentrum = new NakupniCentrum("Zlate Jablko");
+            nakupniCentrum.poplach += VyjezdHasicu;
+            nakupniCentrum.poplach += VyjezdPolicie;
+            nakupniCentrum.poplach -= VyjezdPolicie;
+
+            nakupniCentrum.Pozar();
+            // Doplnte spravne klicove slovo, aby nasledujici radky nesly prelozit
+            // a slo o navrhovy vzor observer
+
+            nakupniCentrum.poplach = VyjezdHasicu;
+            nakupniCentrum.poplach = null;
+            nakupniCentrum.poplach.Invoke("neco");
+
+            
+        }
+    }
+}
