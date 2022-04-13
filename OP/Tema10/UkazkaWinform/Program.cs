a3feb043b07b6d641c1d6902afe653f6875e554b
ekral <ekral@utb.cz>
2022-04-13 12:57:48 +0200

    Create Program.cs

---
+++ OP/Tema10/UkazkaWinform/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    static class Program
+    {
+        static Task<string> GetStringSimulationAsync()
+        {
+            return Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(2000);
+                return $"Ahoj, ted je {DateTime.Now}";
+            });
+        }
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form form = new Form() { Text = "Muj formular" };
+
+            FlowLayoutPanel panel = new FlowLayoutPanel();
+            form.Controls.Add(panel);
+
+            Label label = new Label() { Text = "..." };
+            Button button = new Button() { Text = "Nacti vtip" };
+
+            button.Click += (sender, e) =>
+            {
+                Task<string> task = GetStringSimulationAsync();
+                string text = task.Result;
+
+                label.Text = text;
+            };
+
+            panel.Controls.Add(button);
+            panel.Controls.Add(label);
+
+            Application.Run(form);
+        }
+    }
+}
