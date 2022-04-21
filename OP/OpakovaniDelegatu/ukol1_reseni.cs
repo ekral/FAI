6a3270febca9a625e852fb9b357acfb469f80ec0
ekral <ekral@utb.cz>
2022-04-21 11:50:38 +0200

    Update ukol1_reseni.cs

--- OP/OpakovaniDelegatu/ukol1_reseni.cs
+++ OP/OpakovaniDelegatu/ukol1_reseni.cs
@@ -1,126 +1,69 @@
 using System;
-using System.Collections.Generic;
-using System.Threading;
 
-namespace ConsoleApp31
+namespace ConsoleApp30
 {
-
     class Program
     {
-        private static object locker = new object();
-        private static Timer timer;
-        static void Vypis(double position, List<string> studenti, ConsoleColor pointerColor, ConsoleColor studentColor)
+        static void Metoda1()
         {
-            Console.SetCursorPosition(0, 0);
-
-            for (int i = 0; i < studenti.Count; i++)
-            {
-                if (i == Math.Floor(position))
-                {
-                    Console.ForegroundColor = pointerColor;
-                    Console.Write(" => ");
-                    Console.ForegroundColor = studentColor;
-                    Console.WriteLine(studenti[i] + "        ");
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-                else
-                {
-                    Console.WriteLine("    " + studenti[i] + "        ");
-                }
-            }
+            Console.WriteLine("Ahoj");
         }
+
+        static void Metoda2(int x)
+        {
+            Console.WriteLine(x);
+        }
+
+        static int Metoda3()
+        {
+            return 1;
+        }
+
+        static int Metoda4(string x)
+        {
+            return 1;
+        }
+
+        static int Metoda5(string x, double y)
+        {
+            return 1;
+        }
+
+        static bool Metoda6()
+        {
+            return true;
+        }
+
+        static bool Metoda7(int x)
+        {
+            return x > 0;
+        }
+
+        static bool Metoda8(int x, double y)
+        {
+            return x > y;
+        }
+
+        // Nadefinujte MujTyp1 az MujTyp8 jako typ delegat, kod metody Main nemente
+        delegate void MujTyp1();
+        delegate void MujTyp2(int x);
+        delegate int MujTyp3();
+        delegate int MujTyp4(string x);
+        delegate int MujTyp5(string x, double y);
+        delegate bool MujTyp6();
+        delegate bool MujTyp7(int x);
+        delegate bool MujTyp8(int x, double y);
 
         static void Main(string[] args)
         {
-
-            Random rnd = new Random();
-
-            List<string> studenti = new List<string>
-            {
-                "Kalandrova",
-                "Krivogonov",
-                "Macalik",
-                "Novotny",
-                "Popovsky",
-                "Pozdnyshev",
-                "Prochazka",
-                "Sedlacek",
-                "Uchytil"
-            };
-
-            Console.CursorVisible = false;
-            bool start = true;
-            double speed = 1.0 + rnd.NextDouble();
-            double position = 0;
-            int timeInterval = 25;
-
-
-            timer = new Timer((state) =>
-            {
-                bool hasLock = false;
-
-                try
-                {
-                    Monitor.TryEnter(locker, ref hasLock);
-
-                    if (!hasLock)
-                    {
-                        return;
-                    }
-
-                    timer.Change(Timeout.Infinite, Timeout.Infinite);
-
-                    if (start)
-                    {
-                        start = false;
-                        speed = 1.0 + rnd.NextDouble();
-                    }
-
-                    if (speed > 0.02)
-                    {
-                        Vypis(position, studenti, ConsoleColor.Yellow, ConsoleColor.White);
-
-                        position = (position + speed) % studenti.Count;
-
-                        speed *= 0.99;
-                    }
-                    else
-                    {
-                        Vypis(position, studenti, ConsoleColor.Yellow, ConsoleColor.Red);
-                    }
-                }
-                finally
-                {
-                    if (hasLock)
-                    {
-                        Monitor.Exit(locker);
-                        timer.Change(timeInterval, timeInterval);
-                    }
-
-                }
-
-            }, null, timeInterval, timeInterval);
-
-            bool konec = false;
-            do
-            {
-                ConsoleKey key = Console.ReadKey(true).Key;
-                switch (key)
-                {
-
-                    case ConsoleKey.Escape:
-                        konec = true;
-                        break;
-                    case ConsoleKey.Spacebar:
-                        start = true;
-                        break;
-                    default:
-                        break;
-                }
-
-            } while (!konec);
-
-            timer.Dispose();
+            MujTyp1 d1 = Metoda1;
+            MujTyp2 d2 = Metoda2;
+            MujTyp3 d3 = Metoda3;
+            MujTyp4 d4 = Metoda4;
+            MujTyp5 d5 = Metoda5;
+            MujTyp6 d6 = Metoda6;
+            MujTyp7 d7 = Metoda7;
+            MujTyp8 d8 = Metoda8;
         }
     }
 }
