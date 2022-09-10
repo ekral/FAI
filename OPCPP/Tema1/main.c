68e0b49a124df045de793f9cf2fa70890bf6a3d5
ekral <ekral@utb.cz>
2022-09-10 17:01:23 +0200

    Update main.c

--- OPCPP/Tema1/main.c
+++ OPCPP/Tema1/main.c
@@ -31,12 +31,73 @@ int main()
 
 	// pomoci cyklu while a pote pomoci for vypiste posloupnosti
 	// 1 2 3 4 5 6 7 8 9
+	{
+		int i = 1;
+		while (i < 10)
+		{
+			printf("%d ", i);
+			++i;
+		}
+	}
+	
+	putchar('\n');
+
+	for (int i = 1; i < 10; i++)
+	{
+		printf("%d ", i);
+	}
+
+	putchar('\n');
+
 	// 9 8 7 6 5 4 3 2 1
+	
+	{
+		int i = 9;
+		while (i > 0)
+		{
+			printf("%d ", i);
+			--i;
+		}
+	}
+
+	putchar('\n');
+	
+	for (int i = 9; i > 0; i--)
+	{
+		printf("%d ", i);
+	}
+
+	putchar('\n');
+
 	// 10 100 1000 10000 100000
+	
+	{
+		int i = 10;
+		while (i <= 100000)
+		{
+			printf("%d ", i);
+			i *= 10; // = i * 10
+		}
+	}
+
+	putchar('\n');
+
+	for (int i = 10; i <= 100000; i *= 10)
+	{
+		printf("%d ", i);
+	}
+
+	putchar('\n');
+	
+	int x = 10;
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d ", x);
+		x *= 10;
+	}
+
+	// DU zopakovat struktury a ukazatele
 	// 2 4 6 8 10 12 14 16
 	// 256 128 64 32 16 8 4 2
-	
-	// DU zopakovat struktury a ukazatele
-
 	return 1;
 }
