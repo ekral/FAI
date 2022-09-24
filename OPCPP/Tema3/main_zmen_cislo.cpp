6bcb91de3c5e28cb3ac42ae77015ae7cdbe9054e
ekral <ekral@utb.cz>
2022-09-24 15:32:30 +0200

    Update main_zmen_cislo.cpp

--- OPCPP/Tema3/main_zmen_cislo.cpp
+++ OPCPP/Tema3/main_zmen_cislo.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
+// int* typ ukazatel
 void zmen(int* p)
 {
-	p = 0;
+	*p = 0; // operator indirekce, taky znamy jako operator derefenece
 }
 
 int main()
 {
 	int x = 1;
-	zmen(&x);
+	zmen(&x); // adresni operator
 	printf("x: %d\n", x);
 
 }
