454b05a93688ad1f7a9c32dbccfa9ddfe51b9f8a
ekral <ekral@utb.cz>
2022-10-13 15:38:38 +0200

    Create while_do-while_for.cs

---
+++ PA/Tema4/Prednaska/while_do-while_for.cs
@@ -0,0 +1,25 @@
+int i = 0;
+
+while (i < 10)
+{
+    Console.WriteLine(i);
+    ++i;
+}
+
+for (int k = 0; k < 10; k++) 
+{
+    //Console.WriteLine(k);
+}
+
+// vytvorte vyvojovy diagram pro tento kod:
+int j = 0;
+do
+{
+    Console.WriteLine(j);
+    ++j;
+} while (j < 10);
+
+while(Console.ReadKey().Key != ConsoleKey.Enter)
+{
+
+}
