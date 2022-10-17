96125c81c3d60299b8e2c92e57e24de9241930b8
ekral <ekral@utb.cz>
2022-10-17 09:43:39 +0200

    Update DatabaseService.cs

--- AF/Tema4/Cviceni/DatabaseService.cs
+++ AF/Tema4/Cviceni/DatabaseService.cs
@@ -1,9 +1,10 @@
+using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Data.Sqlite;
 
 namespace SharedProject
 {
@@ -11,7 +12,7 @@ namespace SharedProject
     {
         private const string path = "database.db";
         private readonly string connectionString;
-        
+
         public DatabaseService()
         {
             SqliteConnectionStringBuilder builder = new SqliteConnectionStringBuilder();
@@ -19,6 +20,7 @@ namespace SharedProject
 
             connectionString = builder.ToString();
         }
+
         /// <summary>
         /// Verifies that the database exists. And if it does not exist, it creates a new database.
         /// </summary>
@@ -26,83 +28,108 @@ namespace SharedProject
         {
             if (File.Exists(path)) return false;
 
-            await using var connection = new SqliteConnection(connectionString);
-            connection.Open();
+            await using SqliteConnection connection = new SqliteConnection(connectionString);
+            await connection.OpenAsync();
 
-            var command = connection.CreateCommand();
-            
-            command.CommandText = 
-            @"CREATE TABLE Mortgage 
-            (
-                Id INTEGER PRIMARY KEY,
-                LoanAmount DOUBLE,
-                InterestRate DOUBLE,
-                LoanTerm INTEGER
-            )";
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                CREATE TABLE Mortgage 
+                (
+                    Id INTEGER PRIMARY KEY, 
+                    LoanAmount DOUBLE,
+                    InterestRate DOUBLE,
+                    LoanTerm INTEGER
+                )
+            ";
 
-            await command.ExecuteNonQueryAsync();
+            command.ExecuteNonQuery();
 
             return true;
         }
 
-        public async Task<Model> GetByIdAsync(int id)
-        {
-            throw  new NotImplementedException();
-        }
-
         public async Task<List<Model>> GetAll()
         {
-            await using var connection = new SqliteConnection(connectionString);
-            connection.Open();
+            await using SqliteConnection connection = new SqliteConnection(connectionString);
+            await connection.OpenAsync();
+            
+            SqliteCommand command = connection.CreateCommand();
 
-            var command = connection.CreateCommand();
-            // vrati vsechny radky tabulky Mortgage
-            command.CommandText = "SELECT Id, LoanAmount, InterestRate, LoanTerm FROM Mortgage";
+            command.CommandText =
+            @"
+            SELECT Id, LoanAmount, InterestRate, LoanTerm FROM Mortgage;
+            ";
 
             List<Model> models = new List<Model>();
 
-            var reader = await command.ExecuteReaderAsync();
+            SqliteDataReader reader = await command.ExecuteReaderAsync();
             
             if (reader.HasRows)
             {
                 while (await reader.ReadAsync())
                 {
-                    int id = reader.GetInt32(reader.GetOrdinal("Id"));
-                    double loanAmount = reader.GetDouble(reader.GetOrdinal("LoanAmount"));
-                    double interestRate = reader.GetDouble(reader.GetOrdinal("InterestRate"));
-                    int loanTerm = reader.GetInt32(reader.GetOrdinal("LoanTerm"));
-
-                    Model model = new Model(loanAmount, interestRate, loanTerm) { Id = id };
+                    Model model = new Model();
+                    
+                    model.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+                    model.LoanAmount = reader.GetDouble(reader.GetOrdinal("LoanAmount"));
+                    model.InterestRate = reader.GetDouble(reader.GetOrdinal("InterestRate"));
+                    model.LoanTerm = reader.GetInt32(reader.GetOrdinal("LoanTerm"));
                     
                     models.Add(model);
                 }
             }
+            
             return models;
         }
-        
+
+        public async Task<Model> GetByIdAsync(int id)
+        {
+            
+            throw  new NotImplementedException();
+        }
+
         public async Task InsertAsync(Model model)
         {
-            await using var connection = new SqliteConnection(connectionString);
-            connection.Open();
-
-            var command = connection.CreateCommand();
+            await using SqliteConnection connection = new SqliteConnection(connectionString);
+            await connection.OpenAsync();
+            
+            SqliteCommand command = connection.CreateCommand();
             command.CommandText =
             @"
-             INSERT INTO Mortgage
-             (LoanAmount, InterestRate, LoanTerm)
-             VALUES (@LoanAmount, @InterestRate, @LoanTerm)
+            INSERT INTO Mortgage
+            (LoanAmount, InterestRate, LoanTerm)
+            VALUES (@LoanAmount, @InterestRate, @LoanTerm)
             ";
-
+            
             command.Parameters.Add("@LoanAmount", SqliteType.Real).Value = model.LoanAmount;
             command.Parameters.Add("@InterestRate", SqliteType.Real).Value = model.InterestRate;
             command.Parameters.Add("@LoanTerm", SqliteType.Integer).Value = model.LoanTerm;
-            
+
             int count = command.ExecuteNonQuery();
-        }
+        }   
+
 
         public async Task UpdateAsync(Model model) 
         {
-            throw new NotImplementedException();
+            await using SqliteConnection connection = new SqliteConnection(connectionString);
+            await connection.OpenAsync();
+            
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            UPDATE Mortgage
+            SET LoanAmount = @LoanAmount,
+                InterestRate = @InterestRate,
+                LoanTerm = @LoanTerm
+            WHERE Id = @Id
+            ";
+            
+            command.Parameters.Add("@LoanAmount", SqliteType.Real).Value = model.LoanAmount;
+            command.Parameters.Add("@InterestRate", SqliteType.Real).Value = model.InterestRate;
+            command.Parameters.Add("@LoanTerm", SqliteType.Integer).Value = model.LoanTerm;
+            command.Parameters.Add("@Id", SqliteType.Integer).Value = model.Id;
+
+            int count = command.ExecuteNonQuery();
         }
 
         public void DeleteAsync(Model model) 
