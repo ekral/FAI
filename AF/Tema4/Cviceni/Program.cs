07102ed37af84289ecf7f8a70a5ee230a9ac33e2
ekral <ekral@utb.cz>
2022-10-17 09:43:59 +0200

    Update Program.cs

--- AF/Tema4/Cviceni/Program.cs
+++ AF/Tema4/Cviceni/Program.cs
@@ -1,16 +1,19 @@
-﻿using SharedProject;
+using SharedProject;
 
-DatabaseService service = new DatabaseService();
-if (await service.EnsureCreatedAsync())
+DatabaseService databaseService = new DatabaseService();
+
+if(await databaseService.EnsureCreatedAsync())
 {
-    await service.InsertAsync(new Model(8000000.0, 6.0, 30));
-    await service.InsertAsync(new Model(4000000.0, 5.0, 20));
-    await service.InsertAsync(new Model(10000000.0, 6.0, 30));
+    await databaseService.InsertAsync(new Model(8000000.0, 6.0, 30));
+    await databaseService.InsertAsync(new Model(10800000.0, 5.8, 30));
+    await databaseService.InsertAsync(new Model(4000000.0, 6.2, 20));
 }
 
-List<Model> models = await service.GetAll();
+await databaseService.UpdateAsync(new Model(8800000, 5.9, 25) { Id = 1 });
 
-foreach(Model model in models)
+List<Model> models = await databaseService.GetAll();
+
+foreach (Model m in models)
 {
-    Console.WriteLine($"{model.Id} {model.LoanAmount, 18:C1} {model.InterestRate, 4:F1} {model.LoanTerm, 3}");
+    Console.WriteLine($"{m.Id, 3} {m.LoanAmount, 16:C1} {m.InterestRate, 4:F1} {m.LoanTerm, 3}");
 }
