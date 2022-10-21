fb1f416ab2990d0fc43b73d00da10a40e7bc446e
ekral <ekral@utb.cz>
2022-10-21 16:36:11 +0200

    Update ef_zaklady.cs

--- AF/Tema5/Prednaska/ef_zaklady.cs
+++ AF/Tema5/Prednaska/ef_zaklady.cs
@@ -1,4 +1,6 @@
-// See https://aka.ms/new-console-template for more information
+// https://learn.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=netcore-cli
+// https://learn.microsoft.com/en-us/ef/core/managing-schemas/migrations/managing?tabs=dotnet-core-cli
+
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
