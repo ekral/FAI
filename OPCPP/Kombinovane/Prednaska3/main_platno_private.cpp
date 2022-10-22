9f328eacb803d8bd27b9114279dd0dcac6432416
ekral <ekral@utb.cz>
2022-10-22 09:49:57 +0200

    Update main_platno_private.cpp

--- OPCPP/Kombinovane/Prednaska3/main_platno_private.cpp
+++ OPCPP/Kombinovane/Prednaska3/main_platno_private.cpp
@@ -2,11 +2,11 @@
 
 struct Platno
 {
-	static constexpr int pocetRadku = 20;
+	static constexpr int pocetRadku = 16; // tohle uz mozna nebude static
 	static constexpr int pocetSloupcu = 50;
 	char pozadi;
 
-	char matice[pocetRadku][pocetSloupcu];
+	char matice[pocetRadku][pocetSloupcu]; // Mozna zmenime na jednorozmerne pole na halde
 
 	Platno(char pozadi) : pozadi(pozadi)
 	{
@@ -24,9 +24,24 @@ struct Platno
 		}
 	}
 
+	void Zobraz()
+	{
+		for (int i = 0; i < pocetRadku; i++)
+		{
+			for (int j = 0; j < pocetSloupcu; j++)
+			{
+				putchar(matice[i][j]);
+			}
+
+			putchar('\n');
+		}
+	}
+
 };
 
 int main()
 {
 	Platno platno('-');
+	platno.Zobraz();
+	
 }
