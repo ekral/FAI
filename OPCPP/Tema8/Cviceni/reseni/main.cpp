1bf96a93968cf8594d9f3af59ebf468d99c74d65
ekral <ekral@utb.cz>
2022-11-24 12:23:49 +0100

    Update main.cpp

--- OPCPP/Tema8/Cviceni/reseni/main.cpp
+++ OPCPP/Tema8/Cviceni/reseni/main.cpp
@@ -1,32 +1,35 @@
 #include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <cmath>
+#include <Windows.h>
+#include "Usecka.h"
 #include "Platno.h"
+#include "Algoritmy.h"
 #include "AnimovanaUsecka.h"
-
-void Vypis(Usecka* u) 
-{
-	Bod2d Stred = u->Stred();
-	printf("Usecka: [%lf, %lf] [%lf, %lf] \n", u->P1.x, u->P1.y, u->P2.x, u->P2.y);
-	printf("Stred: [%lf, %lf]\n", Stred.x, Stred.y);
-}
 
 int main()
 {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 2);
+
 	Platno platno(20, 70, '-');
 
-	AnimovanaUsecka animovanaUsecka1(Bod2d(30.0, 10.0), Bod2d(35.0, 15.0), 0.2, 10 * 360.0);
-	AnimovanaUsecka animovanaUsecka2(Bod2d(20.0, 10.0), Bod2d(15.0, 15.0), 0.4,  1 * 360.0);
+	// pridejte druhou usecku
+	AnimovanaUsecka u1(Bod2d(25.0, 8.0), Bod2d(30.0, 12.0), 0.0, 0.2, 10 * 360.0);
 
 	for (int i = 0; i < 10000; i++)
 	{
+
 		platno.Vymaz();
-		platno.NakresliUsecku(animovanaUsecka1.pt1, animovanaUsecka1.pt2, '1');
-		platno.NakresliUsecku(animovanaUsecka2.pt1, animovanaUsecka2.pt2, '1');
+
+		u1.Nakresli(platno);
 
 		platno.Zobraz();
 
-		animovanaUsecka1.DalsiPozice();
-		animovanaUsecka2.DalsiPozice();
+		u1.DalsiPozice();
+
 	}
 
-	int znak = getchar();
+	getchar();
 }
