5aff8910fab62bab353b143f98c3b2c6ac6bef02
Erik Král <ekral@utb.cz>
2023-04-19 13:39:25 +0200

    AvaloniaProject

---
+++ OP/Notebooks/OPN_10_AvaloniaProject/AvaloniaProject/MyWindow.cs
@@ -0,0 +1,58 @@
+﻿using Avalonia.Controls;
+using Avalonia.Layout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaProject
+{
+    class MyWindow : Window
+    {
+        Task<int> DlouhoBeziciMetodaAsync()
+        {
+            return Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(1000);
+                return Random.Shared.Next(0, 100);
+            });
+        }
+
+        public MyWindow()
+        {
+            Title = "AvaloniaUI";
+
+            Button button = new Button()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Content = "Click"
+            };
+
+            TextBlock textBlock = new TextBlock()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Text = "0"
+            };
+
+            button.Click += async (sender, args) =>
+            {
+                int vysledek = await DlouhoBeziciMetodaAsync();
+
+                textBlock.Text = vysledek.ToString();
+            };
+
+            Content = new StackPanel()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Children =
+                {
+                    textBlock,
+                    button
+                }
+            };
+        }
+    }
+}
