56accb0a069f622a0c92702526c532f371561667
ekral <ekral@utb.cz>
2023-10-02 13:45:34 +0200

    Update ascii_malovani.cpp

--- OPCPP/src/ascii_malovani.cpp
+++ OPCPP/src/ascii_malovani.cpp
@@ -14,11 +14,64 @@ struct Bod2d
 	}
 };
 
+class Platno
+{
+private:
+	static constexpr int maxX = 30;
+	static constexpr int maxY = 20;
+	static constexpr int totalChars = maxX * maxY;
+
+	char data[maxX * maxY];
+public:
+	char pozadi;
+	char popredi;
+
+	Platno(char pozadi, char popredi) : pozadi(pozadi), popredi(popredi)
+	{
+		vymaz();
+	}
+
+	void vymaz()
+	{
+		for (int i = 0; i < totalChars; i++)
+		{
+			data[i] = popredi;
+		}
+	}
+
+	void nakresli(Bod2d bod)
+	{
+		// zapise do dat bod dle souradnic
+	}
+
+	void zobraz()
+	{
+		int pos = 0;
+
+		for (int i = 0; i < maxY; i++)
+		{
+			for (int i = 0; i < maxX; i++)
+			{
+				char znak = data[pos];
+				++pos;
+
+				putchar(znak);
+			}
+
+			putchar('\n');
+		}
+	}
+
+
+};
 // main nemente
 int main()
 {
 	Bod2d bod1(2.0, 3.0);
 
-	assert(bod1.x == 2.0);
-	assert(bod1.y == 3.0);
+	Platno platno('O', 'X');
+	platno.nakresli(bod1);
+	platno.zobraz();
 }
+
+
