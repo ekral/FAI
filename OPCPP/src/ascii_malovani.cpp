3abd5e6f1c9c5eb68738ce9ddac2cb7239a19b5e
ekral <ekral@utb.cz>
2023-10-02 13:08:13 +0200

    Create ascii_malovani.cpp

---
+++ OPCPP/src/ascii_malovani.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+
+// doplnte parametricky konstruktor
+// vyuzijte member initializer list
+struct Bod2d
+{
+	double x;
+	double y;
+};
+
+// main nemente
+int main()
+{
+	Bod2d bod1(2.0, 3.0);
+}
