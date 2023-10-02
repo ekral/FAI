e0bffc1584ccd87407fe98b0b5485c3093da8c7e
ekral <ekral@utb.cz>
2023-10-02 13:15:37 +0200

    Update ascii_malovani.cpp

--- OPCPP/src/ascii_malovani.cpp
+++ OPCPP/src/ascii_malovani.cpp
@@ -7,10 +7,18 @@ struct Bod2d
 {
 	double x;
 	double y;
+
+	Bod2d(double x, double y) : x(x), y(y)
+	{
+
+	}
 };
 
 // main nemente
 int main()
 {
 	Bod2d bod1(2.0, 3.0);
+
+	assert(bod1.x == 2.0);
+	assert(bod1.y == 3.0);
 }
