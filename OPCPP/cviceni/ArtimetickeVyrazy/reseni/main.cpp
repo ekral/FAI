cbfbed3590bffdab9e38edbc8def9dea011d0861
ekral <ekral@utb.cz>
2023-10-05 12:22:37 +0200

    Create main.cpp

---
+++ OPCPP/cviceni/ArtimetickeVyrazy/reseni/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main()
+{
+    double a = 5.0;
+    double b = 6.0;
+    double c = 7.0;
+
+    double obsah = 0.0;
+    double obvod = 0.0;
+ 
+    printf("obsah je %lf\n", obsah);
+    printf("obvod je %lf\n", obvod);
+}
