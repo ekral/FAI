19f94ba9bc19526900d403a93a61c67572ab71c6
Erik Král <ekral@utb.cz>
2023-10-09 08:08:16 +0200

    Update Prednaska.Studenti

---
+++ AF/kombinovane/Blok1/Prednaska.Studenti/Prednaska.Studenti/StudentContext.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Prednaska.Studenti
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Jmeno { get; set; }
+        public int SkupinaId { get; set; }
+        public Skupina Skupina { get; set; } // pouze navigation property
+
+        public ICollection<Predmet> Predmety { get; set; }
+
+        public List<Poznamka> Poznamky { get; set; }
+
+    }
+
+    class Poznamka
+    {
+        public int Id { get; set; }
+        public string Poznamka { get; set; }
+    }
+
+    public class Predmet
+    {
+        public int Id { get; set; }
+        public required string Nazev { get; set; }
+
+        public ICollection<Student> Studenti { get; set; }
+
+    }
+
+    class StudentNaPredmetu
+    {
+        public int StudentId { get; set; }
+        public int PredmetId { get; set; }
+    }
+
+    public class Skupina
+    {
+        public int Id { get; set; }
+        public string Nazev { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Studenti { get; set; }
+        public DbSet<Skupina> Skupiny { get; set; }
+
+        private string dbPath = "studenti.db";
+
+        public StudentContext()
+        {
+
+        }
+
+        public StudentContext(string dbPath)
+        {
+            this.dbPath = dbPath;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            string folderPath = Environment.GetFolderPath(folder);
+            string filePath = Path.Join(folderPath, dbPath);
+
+            SqliteConnectionStringBuilder csb = new SqliteConnectionStringBuilder
+            {
+                DataSource = filePath
+            };
+
+            optionsBuilder.UseSqlite(csb.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasMany(s => s.Predmety).WithMany(p => p.Studenti).UsingEntity<StudentNaPredmetu>();
+
+            Skupina skupina = new Skupina() { Id = 1, Nazev = "swi" };
+            Student student = new Student() { Id = 1, Jmeno = "Bohumil", SkupinaId = 1 };
+            Predmet predmet = new Predmet() { Id = 1, Nazev = "Telocvik" };
+
+            StudentNaPredmetu studentNaPredmetu = new StudentNaPredmetu() { StudentId = 1, PredmetId = 1 };
+
+            modelBuilder.Entity<Skupina>().HasData(skupina);
+            modelBuilder.Entity<Student>().HasData(student);
+            modelBuilder.Entity<Predmet>().HasData(predmet);
+            modelBuilder.Entity<StudentNaPredmetu>().HasData(studentNaPredmetu);
+        }
+    }
+}
---
+++ AF/kombinovane/Blok1/Prednsksa/Prednsksa/main.cpp
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+struct Bod2d
+{
+	double x;
+	double y;
+
+	Bod2d(double x, double y) : x(x), y(y)
+	{
+	
+	}
+};
+
+class Platno
+{
+private:
+	static constexpr int columnCount = 30;
+	static constexpr int rowCount = 20;
+	static constexpr int totalChars = columnCount * rowCount;
+	
+	char data[totalChars];
+public:
+	static constexpr int maxColumnIndex = columnCount - 1;
+	static constexpr int maxRowIndex = rowCount - 1;
+	char pozadi;
+	char popredi;
+
+	Platno(char pozadi, char popredi) : pozadi(pozadi), popredi(popredi), data{ 0 }
+	{
+		Vymaz();
+	}
+
+	void Vymaz()
+	{
+		for (int i = 0; i < totalChars; i++)
+		{
+			data[i] = pozadi;
+		}
+	}
+	
+	void NakresliBod(double x, double y)
+	{
+		int pos = ((rowCount - y - 1) * columnCount) + x;
+
+		data[pos] = popredi;
+	}
+
+	void Zobraz()
+	{
+		int pos = 0;
+
+		for (int i = 0; i < rowCount; i++)
+		{
+			for (int j = 0; j < columnCount; j++)
+			{
+				char znak = data[pos];
+				++pos;
+
+				putchar(znak);
+			}
+
+			putchar('\n');
+		}
+	}
+
+};
+
+int main()
+{
+	Bod2d bod(2.0, 3.0);
+
+	Platno platno('-', 'x');
+
+	bool konec = false;
+
+	do
+	{
+		platno.Vymaz();
+		platno.NakresliBod(2, 3);
+
+		platno.popredi = 'O';
+		platno.NakresliBod(0, 0);
+
+		platno.popredi = '1';
+		platno.NakresliBod(platno.maxColumnIndex, 0);
+
+		platno.popredi = '2';
+		platno.NakresliBod(platno.maxColumnIndex, platno.maxRowIndex);
+
+		platno.popredi = '3';
+		platno.NakresliBod(0, platno.maxRowIndex);
+
+		platno.popredi = '-';
+
+		platno.Zobraz();
+
+		// zpracujeme vstupy z klavesnice
+
+		// spocitame nove pozice
+	} while (!konec);
+}
