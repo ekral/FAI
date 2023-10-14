1ce04791159817b974f34b5976121ddd501f9262
ekral <ekral@utb.cz>
2023-10-14 17:28:39 +0200

    Add files via upload

---
+++ AF/src/Utb.PizzaKiosk.UkazkaTestu/Utb.PizzaKiosk.Models/Migrations/20231014124527_PrvniMigrace.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Utb.PizzaKiosk.Models.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrvniMigrace : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pizzas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nazev = table.Column<string>(type: "TEXT", nullable: false),
+                    Cena = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizzas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pizzas");
+        }
+    }
+}
