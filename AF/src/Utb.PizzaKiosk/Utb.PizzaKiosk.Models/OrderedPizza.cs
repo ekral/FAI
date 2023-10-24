e370bb5b6cf740387c74946bf8355eee54d255eb
ekral <ekral@utb.cz>
2023-10-24 10:51:29 +0200

    Add OrderId

--- AF/src/Utb.PizzaKiosk/Utb.PizzaKiosk.Models/OrderedPizza.cs
+++ AF/src/Utb.PizzaKiosk/Utb.PizzaKiosk.Models/OrderedPizza.cs
@@ -3,6 +3,7 @@
     public class OrderedPizza
     {
         public int Id { get; set; }
+        public required int OrderId { get; set; }
         public required string Name { get; set; }
         public required decimal TotalPrice { get; set; }
 
