f41b8431386fc02b1793253ce682d2193b44e506
ekral <ekral@utb.cz>
2023-10-24 09:41:02 +0200

    Added entities

---
+++ AF/src/Utb.PizzaKiosk/Utb.PizzaKiosk.Models/OrderedIngredient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utb.PizzaKiosk.Models
+{
+    public class OrderedIngredient
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required IngredientUnit Unit { get; set; }
+        public int UnitQuantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int OrderedQuantity { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
