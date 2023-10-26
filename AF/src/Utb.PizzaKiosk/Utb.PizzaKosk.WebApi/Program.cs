4669ac909b9c005a5d10aeb786562dedd56ee458
ekral <ekral@utb.cz>
2023-10-26 08:20:35 +0200

    Add Orders WebApi

--- AF/src/Utb.PizzaKiosk/Utb.PizzaKosk.WebApi/Program.cs
+++ AF/src/Utb.PizzaKiosk/Utb.PizzaKosk.WebApi/Program.cs
@@ -4,23 +4,10 @@ using Utb.PizzaKiosk.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<PizzaKioskContext>();
-builder.Services.AddSingleton<IMojeSluzba, MojeSluzba>();
 
 var app = builder.Build();
 
-app.MapGet("/", (PizzaKioskContext context, IMojeSluzba sluzba) => context.Pizzas.Include(p => p.PizzaIngredients).ThenInclude(pi => pi.Ingredient));
+app.MapGet("/", (PizzaKioskContext context) => context.Pizzas.Include(p => p.PizzaIngredients).ThenInclude(pi => pi.Ingredient));
+app.MapGet("/Orders", (PizzaKioskContext context) => context.Orders);
 
 app.Run();
-
-interface IMojeSluzba
-{
-    int VratCislo();
-}
-
-class MojeSluzba : IMojeSluzba
-{
-    public int VratCislo()
-    {
-        return Random.Shared.Next(1, 99);
-    }
-}
