9fb3d699323d5559d9e98ea2d0d770909fec2f7e
Erik Král <ekral@utb.cz>
2023-10-27 09:38:10 +0200

    Add project

---
+++ OPCPP/src/PlatnoPrednaskaKombinovane/PlatnoPrednaskaKombinovane/PlatnoPrednaskaKombinovane.cpp
@@ -0,0 +1,26 @@
+struct Bod2d
+{
+    double x;
+    double y;
+};
+
+class Platno
+{
+private:
+    char data[20 * 30];
+public:
+    void NakresliBod(double x, double y)
+    {
+
+    }
+};
+
+
+int main()
+{
+    // Klientsky kod
+    Platno platno;
+    platno.NakresliBod();
+
+    return 0;
+}
