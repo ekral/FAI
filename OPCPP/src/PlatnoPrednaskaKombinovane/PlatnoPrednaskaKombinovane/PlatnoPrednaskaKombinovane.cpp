12610b8a65e87154a8a44afb753baf2ac3ca369d
Erik Král <ekral@utb.cz>
2023-10-27 09:46:30 +0200

    update main.cpp

--- OPCPP/src/PlatnoPrednaskaKombinovane/PlatnoPrednaskaKombinovane/PlatnoPrednaskaKombinovane.cpp
+++ OPCPP/src/PlatnoPrednaskaKombinovane/PlatnoPrednaskaKombinovane/PlatnoPrednaskaKombinovane.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <math.h>
 
 struct Bod2d
@@ -13,7 +14,16 @@ private:
     int pocetSloupcu = 30;
     int pocetZnaku = pocetRadku * pocetSloupcu;
     char data[20 * 30];
+
 public:
+    void Vymaz()
+    {
+        for (int i = 0; i < pocetZnaku; i++)
+        {
+            data[i] = 'x';
+        }
+    }
+
     void NakresliBod(double x, double y)
     {
         int indexRadku = (int)round(y);
@@ -22,6 +32,24 @@ public:
         int index = indexRadku * pocetSloupcu + indexSloupce;
 
         data[index] = 'o';
+    }
+
+    void Zobraz()
+    {
+        int index = 0;
+
+        for (size_t i = 0; i < pocetRadku; i++)
+        {
+            for (size_t i = 0; i < pocetSloupcu; i++)
+            {
+                char znak = data[index];
+                putchar(znak);
+
+                ++index;
+            }
+
+            putchar('\n');
+        }
     }
 };
 
