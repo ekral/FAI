865213cf5d798096eb89e6646836f333386b504e
Erik Král <ekral@utb.cz>
2023-10-28 17:58:03 +0200

    Add test data and static method

--- AF/src/PujcovnaAutomobilu/PujcovnaAutomobilu/PujcovnaAutomobilu.Tests/DatabaseFixture.cs
+++ AF/src/PujcovnaAutomobilu/PujcovnaAutomobilu/PujcovnaAutomobilu.Tests/DatabaseFixture.cs
@@ -13,7 +13,11 @@ namespace PujcovnaAutomobilu.Tests
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
-            // Add data for test 
+            context.Automobils.AddRange(
+                new Automobil() { Id = 1, Model = "Škoda 105", Pujceno = false },
+                new Automobil() { Id = 2, Model = "Citroen Berlingo", Pujceno = false },
+                new Automobil() { Id = 3, Model = "Škoda Octavia", Pujceno = false }
+            );
         }
 
 
