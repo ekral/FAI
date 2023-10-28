6e72d31d8f581c63fa8706bd41fe5c10375f9d64
Erik Král <ekral@utb.cz>
2023-10-28 15:55:54 +0200

    update

--- AF/src/PujcovnaAutomobilu/PujcovnaAutomobilu/PujcovnaAutomobilu.WebApi/Program.cs
+++ AF/src/PujcovnaAutomobilu/PujcovnaAutomobilu/PujcovnaAutomobilu.WebApi/Program.cs
@@ -1,14 +1,17 @@
 using PujcovnaAutomobilu.Models;
+using PujcovnaAutomobilu.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 builder.Services.AddDbContext<PujcovnaAutomobiluContext>();
 
 var app = builder.Build();
 
 app.MapGet("/", (PujcovnaAutomobiluContext context) => context.Automobils);
+app.MapGet("/Automobil/{id}", (PujcovnaAutomobiluContext context) => context.Automobils);
 
-app.MapGet("/Pujcit/{id}", async (int id, PujcovnaAutomobiluContext context) =>
+app.MapGet("/Pujcit/{id}", async (int id, IEmailSender emailSender, PujcovnaAutomobiluContext context) =>
 {
     Automobil? automobil = await context.Automobils.FindAsync(id);
 
@@ -25,6 +28,8 @@ app.MapGet("/Pujcit/{id}", async (int id, PujcovnaAutomobiluContext context) =>
     automobil.Pujceno = true;
 
     int count = await context.SaveChangesAsync();
+
+    emailSender.SendEmail();
 
     return Results.Ok();
 });
