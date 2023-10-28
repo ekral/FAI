1e47a0d295b5f185c77140bf9a08eb1bad1e9dc3
Erik Král <ekral@utb.cz>
2023-10-28 14:38:54 +0200

    Add Context

---
+++ AF/src/PujcovnaAutomobilu/PujcovnaAutomobilu/PujcovnaAutomobilu.Models/PujcovnaContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Data.Common;
+
+namespace PujcovnaAutomobilu.Models
+{
+    public class PujcovnaContext : DbContext
+    {
+        public PujcovnaContext()
+        {
+
+        }
+
+        public PujcovnaContext(DbContextOptions<PujcovnaContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(optionsBuilder.IsConfigured)
+            {
+                return;
+            }
+
+            Environment.SpecialFolder folder = Environment.SpecialFolder.MyDocuments;
+            string folderPath = Environment.GetFolderPath(folder);
+            string filePath = Path.Join(folderPath, "PujcovnaAutomobilu.db");
+
+            SqliteConnectionStringBuilder builder = new()
+            {
+                DataSource = filePath
+            };
+
+            optionsBuilder.UseSqlite(builder.ConnectionString);
+        }
+
+
+    }
+}
