d780eb4ec2f1d1632072fd010f973bb6b7a06b44
ekral <ekral@utb.cz>
2023-10-31 08:51:47 +0100

    Add EmailSender

---
+++ AF/src/Utb.PizzaKiosk/Utb.PizzaKosk.WebApi/EmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+public class EmailSender : IEmailSender
+{
+    public void SendEmail()
+    {
+        Debug.WriteLine("Opravdu posilam sms");
+    }
+}
--- AF/src/Utb.PizzaKiosk/Utb.PizzaKosk.WebApi/EmailSenderMock.cs
+++ AF/src/Utb.PizzaKiosk/Utb.PizzaKosk.WebApi/EmailSenderMock.cs
@@ -4,6 +4,6 @@ public class EmailSenderMock : IEmailSender
 {
     public void SendEmail()
     {
-        Debug.WriteLine("Posilam sms");
+        Debug.WriteLine("Neposilam sms");
     }
 }
