d0b7192e9693841e78ff9dc1a193fb195d8ca877
ekral <ekral@utb.cz>
2023-10-31 09:00:34 +0100

    Add WebApiVersion1

--- AF/src/Utb.PizzaKiosk/Utb.PizzaKosk.WebApi/Program.cs
+++ AF/src/Utb.PizzaKiosk/Utb.PizzaKosk.WebApi/Program.cs
@@ -1,16 +1,16 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Utb.PizzaKiosk.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Zaregistrujte EmailSender jako singleton
+
 builder.Services.AddDbContext<PizzaKioskContext>();
 
 var app = builder.Build();
 
-app.MapGet("/", (PizzaKioskContext context) => context
-.Pizzas
-    .Include(p => p.PizzaIngredients)
-    .ThenInclude(pi => pi.Ingredient));
+app.MapGet("/", WebApiVersion1.AllPizzas);
 
 app.MapGet("/Orders", (PizzaKioskContext context) => 
 context
@@ -20,3 +20,16 @@ context
 
 app.Run();
 
+public static class WebApiVersion1
+{
+    public static async Task<Ok<Pizza[]>> AllPizzas(PizzaKioskContext context)
+    {
+        Pizza[] pizzas = await context
+            .Pizzas
+            .Include(p => p.PizzaIngredients)
+            .ThenInclude(pi => pi.Ingredient).ToArrayAsync();
+
+
+        return TypedResults.Ok(pizzas);
+    }
+}
