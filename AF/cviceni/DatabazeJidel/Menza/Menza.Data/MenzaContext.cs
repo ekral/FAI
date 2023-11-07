64e2b227b0c66547223271fe495bb1fd1c62c6c9
ekral <ekral@utb.cz>
2023-11-07 09:43:52 +0100

    Add menza

---
+++ AF/cviceni/DatabazeJidel/Menza/Menza.Data/MenzaContext.cs
@@ -0,0 +1,40 @@
+﻿using Menza.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Menza.Data
+{
+    public class MenzaContext : DbContext
+    {
+        public DbSet<Jidlo> Jidla { get; set; }
+        
+
+        public MenzaContext()
+        {
+
+        }
+
+        public MenzaContext(DbContextOptions<MenzaContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                return;
+            }
+
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var folderPath = Environment.GetFolderPath(folder);
+            string filePath = Path.Join(folderPath, "Menza.db");
+
+            SqliteConnectionStringBuilder stringBuilder = new()
+            {
+                DataSource = filePath
+            };
+
+            optionsBuilder.UseSqlite(stringBuilder.ConnectionString);
+        }
+    }
+}
---
+++ AF/cviceni/DatabazeJidel/Menza/Menza.Models/Jidlo.cs
@@ -0,0 +1,9 @@
+﻿namespace Menza.Models
+{
+    public class Jidlo
+    {
+        public required int Id {get; set;}
+        public required string Nazev { get; set; }
+        public required int Cena { get; set; }
+    }
+}
