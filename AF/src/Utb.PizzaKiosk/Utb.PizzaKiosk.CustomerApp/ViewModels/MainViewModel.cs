e162681b733bb95af3a460b043098d4f2ff2773b
Erik Král <ekral@utb.cz>
2023-11-07 14:00:35 +0100

    Update client

--- AF/src/Utb.PizzaKiosk/Utb.PizzaKiosk.CustomerApp/ViewModels/MainViewModel.cs
+++ AF/src/Utb.PizzaKiosk/Utb.PizzaKiosk.CustomerApp/ViewModels/MainViewModel.cs
@@ -1,6 +1,35 @@
-﻿namespace Utb.PizzaKiosk.CustomerApp.ViewModels;
+﻿using ReactiveUI;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Reactive.Concurrency;
+using Utb.PizzaKiosk.Models;
+
+namespace Utb.PizzaKiosk.CustomerApp.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private Pizza? _pizza;
+
+    public Pizza? Pizza
+    {
+        get => _pizza;
+        set => this.RaiseAndSetIfChanged(ref _pizza, value);
+    }
+
+    public MainViewModel()
+    {
+        RxApp.MainThreadScheduler.Schedule(LoadPizza);
+    }
+
+    private async void LoadPizza()
+    {
+        Pizza? pizza = await App.Client.GetFromJsonAsync<Pizza>(@"https://localhost:7149/Pizza/2");
+
+        if(pizza is not null)
+        {
+            Pizza = pizza;
+        }
+    }
 }
+
+
