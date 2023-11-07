f0eb16471d8636926438e56a6a49c7bcae2df16d
ekral <ekral@utb.cz>
2023-11-07 09:53:24 +0100

    Add seeder

---
+++ AF/cviceni/DatabazeJidel/Menza/Menza.DataSeeder/Program.cs
@@ -0,0 +1,14 @@
+﻿using Menza.Data;
+using Menza.Models;
+
+using var context = new MenzaContext();
+
+context.Database.EnsureDeleted();
+context.Database.EnsureCreated();
+
+context.Jidla.AddRange(
+    new Jidlo() { Id = 1, Nazev = "Pizza", Cena = 150},
+    new Jidlo() { Id = 2, Nazev = "Pizza2", Cena = 200},
+    new Jidlo() { Id = 3, Nazev = "Pizza3", Cena = 250}
+);
+context.SaveChanges();
