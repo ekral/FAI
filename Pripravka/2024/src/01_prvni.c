dc0639713940ebb172ddfe49234f94710b9204f3
Erik Král <ekral@utb.cz>
2024-08-20 08:54:23 +0200

    clean example

--- Pripravka/2024/src/01_prvni.c
+++ Pripravka/2024/src/01_prvni.c
@@ -9,6 +9,7 @@
 int main()
 {
     int x = 5; // int je cele cislo se znamenkem
+    
     printf("hodnota x: %d\n", x);
     printf("adresa x: %p\n", &x);
     printf("pocet bytu: %llu\n", sizeof(x)); // nemusite vedet
@@ -18,15 +19,5 @@ int main()
     printf("hodnota hmotnost: %lf\n", hmotnost);
     printf("adresa hmotnost: %p\n", &hmotnost);
     printf("pocet bytu: %llu\n", sizeof(hmotnost));
-
-    // TODO scanf, prumer tri int, obvod a obsah ctverce, obdelniku, kruhu a trojuhelnika
-    // https://github.com/ekral/FAI/tree/master/Pripravka/2024
-    double vyskaMetry = 1.86;
-
-    // operator deleno je /, napriklad a / b
-    // operator nasobeni je *, napriklad a * a
-    double bmi = hmotnost / (vyskaMetry * vyskaMetry);
-
-    printf("bmi: %lf\n", bmi);
 }
 
