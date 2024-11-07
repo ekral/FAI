cf2cd9133bda93da8a075f9440262ce85b33d579
ekral <ekral@utb.cz>
2024-11-07 10:55:17 +0100

    add reseni

---
+++ OPCPP/cviceni/Zasobnik/reseni/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <assert.h>
+#include <algorithm>
+class Stack
+{
+private:
+    int n;
+    int* data;
+    int pos;
+public:
+    explicit Stack(const int n) : n(n), data(new int[n]), pos(0)
+    {
+    }
+    
+    // kopirovaci konstruktor
+    Stack(const Stack& other) : n(other.n), data(new int[n]), pos(0)
+    {
+        std::copy_n(other.data, n, data);
+    }
+
+    ~Stack()
+    {
+        delete[] data;
+    }
+
+    void push(const int x)
+    {
+        assert(pos < n);
+
+        data[pos] = x;
+        ++pos;
+    }
+
+    int pop()
+    {
+        assert(pos > 0);
+
+        --pos;
+        return data[pos];
+    }
+
+    int operator[] (const int i) const
+    {
+        return data[i];
+    }
+
+    int count() const
+    {
+        return pos;
+    }
+};
+
+void print(const Stack& stack)
+{
+    for(int i = 0; i < stack.count(); ++i)
+    {
+        std::cout << stack[i] << std::endl;
+    }
+}
+
+int main()
+{
+    Stack stack(10);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    Stack copy = stack;
+
+    print(stack);
+
+    std::cout << stack.pop() << std::endl;
+    std::cout << stack.pop() << std::endl;
+    std::cout << stack.pop() << std::endl;
+
+    std::cout << stack.count() << std::endl;
+    return 0;
+}
