5e0fc433c2221e2107acf802d0903688c24fab4d
Erik Král <ekral@utb.cz>
2025-01-06 12:30:23 +0100

    add InventoryTracker

---
+++ Ruzne/UTB.InventoryTracker/WebAPI/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace WebAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        required public string Name { get; set; }
+        required public string Description { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        required public string Name { get; set; }
+        required public string Description { get; set; }
+        required public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        required public decimal Price { get; set; }
+        required public string StockKeepingUnit { get; set; }
+    }
+
+
+}
