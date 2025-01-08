872942bf0a81ebaf84eb8f81cf7cb9e3de237c62
Erik Král <ekral@utb.cz>
2025-01-08 16:12:29 +0100

    Update Product and add Transaction and TransactionType

---
+++ Ruzne/UTB.InventoryTracker/WebAPI/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public required int ProductId { get; set; }
+        public required int TransactionTypeId { get; set; }
+        public required int QuantityChanged { get; set; }
+        public required DateTime TransactionDate { get; set; }
+        public Product? Product { get; set; }
+        public TransactionType? TransactionType { get; set; }
+    }
+}
---
+++ Ruzne/UTB.InventoryTracker/WebAPI/Models/TransactionType.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class TransactionType
+    {
+        public int TransactionTypeId { get; set; }
+
+        public required string Name { get; set; }
+    }
+}
