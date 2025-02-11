a528e3e862b0507f83ab254fc8e0d4b556417d53
Erik Král <ekral@utb.cz>
2025-02-11 15:45:04 +0100

    move ado net

---
+++ Ruzne/BlazorApp2/BlazorApp2/BlazorApp2.Client/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorApp2.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ Ruzne/MauiApp1/MauiApp1/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
