8e2de1f596ee9a86c8f8166346972b8ca78b31a3
ekral <ekral@utb.cz>
2025-03-05 09:39:41 +0100

    Update Program.cs

--- AF/Cviceni/EntityFrameworkRelace/reseni/Program.cs
+++ AF/Cviceni/EntityFrameworkRelace/reseni/Program.cs
@@ -1,16 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
 
-namespace MujDvanactyProjekt
+namespace MojeCtvrtaAplikace
 {
-    // 1 skupina : n studentu
-
     class Student
     {
         public int StudentId { get; set; }
         public required string Jmeno { get; set; }
         public int SkupinaId { get; set; } // Cizi klic
-        public Skupina? Skupina { get; set; } // Navigation property
-        public Karta? Karta { get; set; }
+        public Skupina? Skupina { get; set; } // Navigation Property
+        public Karta? Karta { get; set; } // Navigation Property
         public List<Predmet> Predmety { get; set; } = [];
     }
 
@@ -31,15 +30,15 @@ namespace MujDvanactyProjekt
     {
         public int KartaId { get; set; }
         public decimal Zustatek { get; set; }
-        public int StudentId { get; set; }
-        public Student? Student { get; set; }
-    }
+        public int StudentId { get; set; } // Cizi klic
+        public Student? Student { get; set; } // Navigation Property
 
+    }
     class Skupina
     {
         public int SkupinaId { get; set; }
         public required string Nazev { get; set; }
-        public List<Student> Studenti { get; set; } = []; // Collection Navigation property
+        public List<Student> Studenti { get; set; } = [];
     }
 
     class SkolaContext(DbContextOptions<SkolaContext> options) : DbContext(options)
@@ -82,20 +81,20 @@ namespace MujDvanactyProjekt
             Skupina skupina1 = new() { SkupinaId = 1, Nazev = "SWI1" };
             Skupina skupina2 = new() { SkupinaId = 2, Nazev = "SWI2" };
 
-            Student student1 = new() { StudentId = 1, Jmeno = "Martin", SkupinaId = 1 };
-            Student student2 = new() { StudentId = 2, Jmeno = "Alena", SkupinaId = 2 };
-            Student student3 = new() { StudentId = 3, Jmeno = "Petr", SkupinaId = 2 };
+            Student student1 = new() { StudentId = 1, Jmeno = "Kazimir", SkupinaId = 1 };
+            Student student2 = new() { StudentId = 2, Jmeno = "Fridrich", SkupinaId = 1 };
+            Student student3 = new() { StudentId = 3, Jmeno = "Alena", SkupinaId = 2 };
 
             Karta karta1 = new() { KartaId = 1, Zustatek = 200.0m, StudentId = 1 };
             Karta karta2 = new() { KartaId = 2, Zustatek = 600.0m, StudentId = 2 };
-            Karta karta3 = new() { KartaId = 3, Zustatek = 400.0m, StudentId = 3 };
+            Karta karta3 = new() { KartaId = 3, Zustatek = 800.0m, StudentId = 3 };
 
             Predmet predmet1 = new() { PredmetId = 1, Nazev = "Elektro"};
-            Predmet predmet2 = new() { PredmetId = 2, Nazev = "Tenis"};
+            Predmet predmet2 = new() { PredmetId = 2, Nazev = "badminton"};
 
-            PredmetStudent ps1 = new() { PredmetId = 1, StudentId = 1 };
-            PredmetStudent ps2 = new() { PredmetId = 2, StudentId = 1 };
-            PredmetStudent ps3 = new() { PredmetId = 2, StudentId = 2 };
+            PredmetStudent ps1 = new() { StudentId = 1, PredmetId = 1 };
+            PredmetStudent ps2 = new() { StudentId = 2, PredmetId = 2 };
+            PredmetStudent ps3 = new() { StudentId = 3, PredmetId = 2 };
 
             await context.Skupiny.AddRangeAsync(skupina1, skupina2);
             await context.Studenti.AddRangeAsync(student1, student2, student3);
@@ -110,23 +109,14 @@ namespace MujDvanactyProjekt
         {
             await using SkolaContext context = CreateContext();
 
-            Console.WriteLine("Skupiny:");
-
-            foreach (Skupina skupina in await context.Skupiny
-                .Include(sk => sk.Studenti)
-                .ThenInclude(st => st.Karta)
-                .ToListAsync())
+            foreach (Skupina skupina in await context.Skupiny.Include(sk => sk.Studenti).ToListAsync())
             {
                 Console.WriteLine($"{skupina.SkupinaId} {skupina.Nazev}");
+                Console.WriteLine($"  {skupina.Studenti.Count}");
 
                 foreach (Student student in skupina.Studenti)
                 {
                     Console.WriteLine($"  {student.Jmeno}");
-
-                    if(student.Karta is not null)
-                    {
-                        Console.WriteLine($"    zustatek: {student.Karta.Zustatek}");
-                    }
                 }
             }
         }
@@ -135,24 +125,33 @@ namespace MujDvanactyProjekt
         {
             await using SkolaContext context = CreateContext();
 
-            Console.WriteLine("Skupiny Explicit:");
-
             foreach (Skupina skupina in await context.Skupiny.ToListAsync())
             {
                 Console.WriteLine($"{skupina.SkupinaId} {skupina.Nazev}");
 
                 await context.Entry(skupina).Collection(sk => sk.Studenti).LoadAsync();
 
+                Console.WriteLine($"  {skupina.Studenti.Count}");
+
                 foreach (Student student in skupina.Studenti)
                 {
                     Console.WriteLine($"  {student.Jmeno}");
+                }
+            }
+        }
 
-                    await context.Entry(student).Reference(st => st.Karta).LoadAsync();
+        public static async Task VypisStudentu()
+        {
+            await using SkolaContext context = CreateContext();
 
-                    if (student.Karta is not null)
-                    {
-                        Console.WriteLine($"    zustatek: {student.Karta.Zustatek}");
-                    }
+            foreach (Student student in await context.Studenti.Include(sk => sk.Predmety).ToListAsync())
+            {
+                Console.WriteLine($"{student.Jmeno} {student.StudentId}");
+                Console.WriteLine($"  {student.Predmety.Count}");
+
+                foreach (Predmet predmet in student.Predmety)
+                {
+                    Console.WriteLine($"  {predmet.Nazev}");
                 }
             }
         }
@@ -162,6 +161,7 @@ namespace MujDvanactyProjekt
             await Seed();
             await VypisSkupinyEager();
             await VypisSkupinyExplicit();
+            await VypisStudentu();
         }
     }
 }
