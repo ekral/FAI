5cea2814af1e449296bb2e55fd8f5ed8a9bdc945
Erik Král <ekral@utb.cz>
2025-04-04 17:57:48 +0200

    Refactor student API and enhance UI with QuickGrid
    - Encapsulated student-related API endpoints in a new `StudentApi` class.
    - Added new navigation links for "QuickGrid 1" and "QuickGrid 2".
    - Introduced `QuickGridMemory.razor` and `QuickGridMemoryPaging.razor` components with search and pagination features.
    - Enhanced CSS for grid components and streamlined the `EditStudent.razor` interface.

--- AF/notebooks/08_blazor_quickgrid_src/Studenti/Studenti.WebAPI/Program.cs
+++ AF/notebooks/08_blazor_quickgrid_src/Studenti/Studenti.WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
+using Studenti.WebAPI.Apis;
 using Studenti.WebAPI.Data;
 using Studenti.WebAPI.Models;
 
@@ -19,71 +20,9 @@ namespace Studenti.WebAPI
 
             app.UseCors(p => p.WithOrigins("https://localhost:7235").AllowCredentials().AllowAnyMethod().AllowAnyHeader()); // CORS 
 
-            app.MapGet("/seed", Seed);
-            app.MapGet("/students", GetAllStudents);
-            app.MapGet("/students/{id}", GetStudentById);
-            app.MapPut("/students/{id}", UpdateStudent);
+            app.MapStudentApi();
 
             app.Run();
-        }
-
-        public static async Task<Created> Seed(StudentContext context)
-        {
-            await context.Database.EnsureDeletedAsync();
-
-            if (await context.Database.EnsureCreatedAsync())
-            {
-                string[] names = ["Jiri", "Karel", "Alena", "Petr", "Samuel", "Tereza", "Marko", "David", "Jan", "Filip", "Martin", "Erik", "Jana", "Ondrej"];
-
-                for (int i = 0; i < 128; i++)
-                {
-                    string jmeno = names[Random.Shared.Next(names.Length - 1)];
-                    bool studuje = Random.Shared.NextDouble() >= 0.5;
-
-                    Student novy = new Student() { Jmeno = jmeno, Studuje = studuje };
-
-                    await context.AddAsync(novy);
-                }
-
-                await context.SaveChangesAsync();
-            }
-
-            return TypedResults.Created();
-        }
-
-        public static async Task<Ok<Student[]>> GetAllStudents(StudentContext context)
-        {
-            return TypedResults.Ok(await context.Studenti.ToArrayAsync());
-        }
-
-
-        public static async Task<Results<Ok<Student>, NotFound>> GetStudentById(int id, StudentContext context)
-        {
-            if (await context.Studenti.FindAsync(id) is Student student)
-            {
-                return TypedResults.Ok(student);
-            }
-            else
-            {
-                return TypedResults.NotFound();
-            }
-        }
-
-        public static async Task<Results<NoContent, NotFound>> UpdateStudent(int id, Student input, StudentContext context)
-        {
-            if (await context.Studenti.FindAsync(id) is Student student)
-            {
-                student.Jmeno = input.Jmeno;
-                student.Studuje = input.Studuje;
-        
-                await context.SaveChangesAsync();
-        
-                return TypedResults.NoContent();
-            }
-            else
-            {
-                return TypedResults.NotFound();
-            }
         }
     }
 }
