d4bb8212747ca5a347089806ae9a038ce0f959b8
Erik Král <ekral@utb.cz>
2025-04-14 09:27:36 +0200

    Add initial Visual Studio solution file for Studenti project
    - Created a new solution file for the Studenti project containing two projects: Studenti.WebAPI and Studenti.WebClient.
    - Configured solution properties, including build configurations for Debug and Release.
    - Added project GUIDs and solution GUID for proper identification within Visual Studio.

---
+++ AF/cviceni/08_quick_grid/reseni/Studenti/Studenti.WebAPI/Data/StudentContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Studenti.WebAPI.Models;
+
+namespace Studenti.WebAPI.Data
+{
+    public class StudentContext(DbContextOptions<StudentContext> options) : DbContext(options)
+    {
+        public DbSet<Student> Studenti { get; set; }
+    }
+}
