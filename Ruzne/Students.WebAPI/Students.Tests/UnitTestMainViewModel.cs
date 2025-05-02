fe2631159f5a46b4a3290645435b7c7e55416309
Erik Král <ekral@utb.cz>
2025-05-02 15:43:31 +0200

    minor

---
+++ Ruzne/Students.WebAPI/Students.Tests/UnitTestMainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Students.AvaloniaApp.Services;
+using Students.AvaloniaApp.ViewModels;
+
+class StudentService : IStudentService
+{
+    private StudentViewModel[] Students { get; } =
+        [
+            ne
+        ]
+    public Task<StudentViewModel[]?> GetAllStudentsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateStudentAsync(StudentViewModel student)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+namespace Students.Tests
+{
+    public class UnitTestMainViewModel
+    {
+        [Fact]
+        public void Test1()
+        {
+            MainViewModel viewModel = new MainViewModel()
+        }
+    }
+}
