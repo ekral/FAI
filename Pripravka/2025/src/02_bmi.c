2da5f2f226c3656d75a81c2619e91417315a5682
ekral <ekral@utb.cz>
2025-08-18 12:51:14 +0200

    Update 02_bmi.c

--- Pripravka/2025/src/02_bmi.c
+++ Pripravka/2025/src/02_bmi.c
@@ -2,21 +2,38 @@
 
 int main()
 {
-    double hmotnost = 0.0;
-    double vyskaCentimetry = 0.0;
+	// = je operator prirazeni
+	double hmotnost = 96.0;
+	double vyska = 1.86;
 
-    printf("Zadej hmotnost: ");
-    // funce scanf_s ma jako argument adresu kam ma ulozit vysledek
-    scanf_s("%lf", &hmotnost); 
+	double bmi = hmotnost / (vyska * vyska);
 
-    // Ukol: prepracujte zadavani na vysku v centimetrech
-    printf("Zadej vysku v centimetrech: ");
-    scanf_s("%lf", &vyskaCentimetry);
+	printf("vyska: %.2lF cm\n", vyska * 100);
+	printf("hmotnost: %.2lF kg\n", hmotnost);
+	printf("bmi: %.2lF\n", bmi);
 
-    double vyskaMetry = vyskaCentimetry / 100.0;
+	// spocitejte nejmensi a nejvetsi doporucenou
+	// doporucenou hmotnost dle vysky
 
-    double bmi = hmotnost / (vyskaMetry * vyskaMetry);
+	double min_bmi = 18.5;
+	double min_hmotnost = min_bmi * (vyska * vyska);
 
-    printf("bmi: %lf\n", bmi);
+	printf("minimalni zdrava hmotnost: %.0lF\n", min_hmotnost);
+
+	// spocitejte a vypiste maximalni zdravou hmostnost
+
+	double max_bmi = 25.0;
+	double max_hmotnost = max_bmi * (vyska * vyska);
+
+	printf("maximalni zdrava hmotnost: %.0lF\n", max_hmotnost);
+
+	// vypiste o kolik kilogramu by mela osoba zhubnout
+	// pokud vazi mene tak vypise zaporne cislo
+
+	// - je operator odcitani
+	double rozdil = hmotnost - max_hmotnost;
+
+	printf("Mel by jsi zhubnout: %.2lF\n", rozdil);
+
+	return 0;
 }
-
