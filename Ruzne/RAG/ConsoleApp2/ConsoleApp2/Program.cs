533d9619aef5528b0a55f96cc027da9cb46d3d65
Erik Král <ekral@utb.cz>
2025-09-30 09:28:31 +0200

    minor fix

---
+++ Ruzne/RAG/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp2
+{
+
+    internal class Program
+    {
+        static void Zmen(int[] pole)
+        {
+            pole = [0];
+        }
+
+        static void Main(string[] args)
+        {
+            int[] pole = [1, 2, 3];
+            Zmen(pole);    
+
+            System.Console.WriteLine(string.Join(",", pole));
+        }
+    }
+}
