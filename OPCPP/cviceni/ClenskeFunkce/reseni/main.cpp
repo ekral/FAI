3cb08a8f702c4ee60d4092ed507251f46b425bd8
ekral <ekral@utb.cz>
2025-10-02 14:37:12 +0200

    Create main.cpp

---
+++ OPCPP/cviceni/ClenskeFunkce/reseni/main.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<math.h>
+class Hypoteka
+{
+public:
+	double p;
+	double urokProcenta;
+	int pocetLet;
+
+	double Splatka()
+	{
+		double r = urokProcenta / 100 / 12;
+		int n = pocetLet * 12;
+		return (p * r * pow(1 + r, n)) / (pow(1 + r, n) - 1);
+	}
+};
+
+void ZadejHodnoty(Hypoteka* hypoteka)
+{
+	int ret;
+	
+	puts("Zadej vysi pujcky");
+	ret = scanf_s("%lf", &hypoteka->p);
+
+	puts("Zadej urok");
+	ret = scanf_s("%lf", &hypoteka->urokProcenta);
+
+	puts("Zadej pocet let");
+	ret = scanf_s("%d", &hypoteka->pocetLet);
+}
+
+void Vypis(Hypoteka* hypoteka)
+{
+	printf("pujcka: %.2lf urok: %.2lf pocet let: %d\n", hypoteka->p, hypoteka->urokProcenta, hypoteka->pocetLet);
+}
+
+int main()
+{
+	Hypoteka h1;
+	Hypoteka h2;
+
+	ZadejHodnoty(&h1);
+	ZadejHodnoty(&h2);
+	
+	Vypis(&h1);
+	printf("Vyse splatky 1: %lf\n", h1.Splatka());
+	Vypis(&h2);
+	printf("Vyse splatky 2: %lf\n", h2.Splatka());
+}
