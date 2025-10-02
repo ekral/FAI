b9f644011d41957dbaa79bc1bf8f16776cee31e7
ekral <ekral@utb.cz>
2025-10-02 14:35:42 +0200

    Update reseni_splatka_hypoteky.cpp

--- OPCPP/cviceni/ArtimetickeVyrazy/reseni/reseni_splatka_hypoteky.cpp
+++ OPCPP/cviceni/ArtimetickeVyrazy/reseni/reseni_splatka_hypoteky.cpp
@@ -1,28 +1,51 @@
 #include <stdio.h>
-#define _USE_MATH_DEFINES
-#include <math.h>
+#include<math.h>
+class Hypoteka
+{
+public:
+	double p;
+	double urokProcenta;
+	int pocetLet;
+
+	double Splatka()
+	{
+		double r = urokProcenta / 100 / 12;
+		int n = pocetLet * 12;
+		return (p * r * pow(1 + r, n)) / (pow(1 + r, n) - 1);
+	}
+};
+
+void ZadejHodnoty(Hypoteka* hypoteka)
+{
+	int ret;
+	
+	puts("Zadej vysi pujcky");
+	ret = scanf_s("%lf", &hypoteka->p);
+
+	puts("Zadej urok");
+	ret = scanf_s("%lf", &hypoteka->urokProcenta);
+
+	puts("Zadej pocet let");
+	ret = scanf_s("%d", &hypoteka->pocetLet);
+}
+
+void Vypis(Hypoteka* hypoteka)
+{
+	printf("pujcka: %.2lf urok: %.2lf pocet let: %d\n", hypoteka->p, hypoteka->urokProcenta, hypoteka->pocetLet);
+}
 
 int main()
 {
-    double p = 6000000.0;
-    int rocniUrokProcenta = 6.0;
-    int pocetLet = 30;
+	Hypoteka h1;
+	Hypoteka h2;
 
-    double r = (rocniUrokProcenta/100.0)/12;
-    int n = pocetLet * 12;
-
-    double m = (p * r * (pow(1+r,n)))/ (pow(1+r,n)-1);
-
-    printf("splatka je %lf\n", m); // 35973.03 
-
-    while(p > 0)
-    {
-        double nom = r * p;
-        double umor = m - nom;
-        p = p - umor;
-        printf("aktualni dluh %lf\t", p);
-        printf("nominalni vyse uroku %lf\t", nom);
-        printf("Umor %lf\n", umor);
-    }
-    
+	ZadejHodnoty(&h1);
+	ZadejHodnoty(&h2);
+	
+	Vypis(&h1);
+	printf("Vyse splatky 1: %.2lf\n", h1.Splatka());
+	Vypis(&h2);
+	printf("Vyse splatky 2: %.2lf\n", h2.Splatka());
 }
+
+
