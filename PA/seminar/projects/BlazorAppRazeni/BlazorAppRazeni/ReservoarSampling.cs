f7a0b9881c4458b74879bba7112a829cf66a7bb1
Erik Král <ekral@utb.cz>
2025-10-15 07:09:13 +0200

    Add reservoar sampling static method

---
+++ PA/seminar/projects/BlazorAppRazeni/BlazorAppRazeni/ReservoarSampling.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorAppRazeni
+{
+    public static class ReservoarSampling
+    {
+        public static int[] Generate(int n, int max)
+        {
+            int[] pole = new int[n];
+
+            for (int i = 0; i < max; ++i)
+            {
+                if (i < n)
+                {
+                    pole[i] = i + 1;
+                }
+                else
+                {
+                    int randomIndex = Random.Shared.Next(0, i + 1);
+
+                    if (randomIndex < n)
+                    {
+                        pole[randomIndex] = i + 1;
+                    }
+                }
+            }
+
+            return pole;
+        }
+    }
+}
