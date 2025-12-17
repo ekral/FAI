215694d8e48d63a1100832e8744b37922b6f0ab2
ekral <ekral@utb.cz>
2025-12-17 08:49:41 +0100

    add QuickSort

---
+++ PA/seminar/zadani/CodeClash3/reseni/BlazorAppBattle/BlazorAppBattle/Algorithms/QuickSort.cs
@@ -0,0 +1,94 @@
+﻿namespace BlazorAppBattle.Algorithms
+{
+    public class QuickSort
+    {
+        public struct Partition(int low, int high)
+        {
+            public int Low = low;
+            public int High = high;
+        }
+
+        public int Rank { get; set; }
+        public int[] Array { get; private set; }
+        public bool IsSorted { get; private set; }
+        public int CurrentIndex { get; private set; }
+        public int PlacementIndex { get; private set; }
+        public int Low { get; private set; }
+        public int High { get; private set; }
+        public int Pivot { get; private set; }
+        public Queue<Partition> Partitions { get; private set; } = [];
+
+        public QuickSort(int[] array)
+        {
+            Rank = 0;
+            Array = array;
+            IsSorted = false;
+            CurrentIndex = 0;
+            PlacementIndex = 0;
+            Low = 0;
+
+            if (Array.Length < 2)
+            {
+                IsSorted = true;
+            }
+
+            High = Array.Length - 1;
+            Pivot = Array[High];
+        }
+
+        public void NextIteration()
+        {
+            if (IsSorted)
+            {
+                return;
+            }
+
+            if(CurrentIndex < High)
+            {
+                if (Array[CurrentIndex] <= Pivot)
+                {
+                    Tools.Swap(Array, CurrentIndex, PlacementIndex);
+
+                    ++PlacementIndex;
+                }
+
+                ++CurrentIndex;
+            }
+            else
+            {   
+                Tools.Swap(Array, CurrentIndex, PlacementIndex); // umisteni pivota
+
+                // PlacementIndex je index Pivota
+
+                if (Low < PlacementIndex - 1)
+                {
+                    Partition left = new(Low, PlacementIndex - 1);
+
+                    Partitions.Enqueue(left);
+                }
+
+                if (PlacementIndex + 1 < High)
+                {
+                    Partition right = new(PlacementIndex + 1, High);
+
+                    Partitions.Enqueue(right);
+                }
+
+                if(Partitions.Count > 0)
+                {
+                    Partition partition = Partitions.Dequeue();
+
+                    Low = partition.Low;
+                    High = partition.High;
+                    CurrentIndex = Low;
+                    PlacementIndex = Low;
+                    Pivot = Array[High];
+                }
+                else
+                {
+                    IsSorted = true;
+                }
+            }
+        }
+    }
+}
