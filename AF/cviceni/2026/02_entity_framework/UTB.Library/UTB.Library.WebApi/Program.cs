e2d05f90d6fc2a3d6421e1fb77d5ee7e9e7f4de0
ekral <ekral@utb.cz>
2026-02-17 13:07:07 +0100

    Add http file

--- AF/cviceni/2026/02_entity_framework/UTB.Library/UTB.Library.WebApi/Program.cs
+++ AF/cviceni/2026/02_entity_framework/UTB.Library/UTB.Library.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using UTB.Library.Contracts;
@@ -15,15 +16,29 @@ app.MapDefaultEndpoints();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/weatherforecast", () =>
-{
-    
-});
+app.MapPost("/authors", WebApiVersion1.CreateAuthor);
+app.MapGet("/authors", WebApiVersion1.GetAllAuthors);
+app.MapGet("/authors/{id:int}", WebApiVersion1.GetAuthor);
+app.MapPut("/authors/{id:int}", WebApiVersion1.UpdateAuthor);
+app.MapDelete("/authors/{id:int}", WebApiVersion1.DeleteAuthor);
 
 app.Run();
 
 public static class WebApiVersion1
 {
+    public static async Task<Created<AuthorDto>> CreateAuthor(AuthorDto authorDto, LibraryContext context)
+    {
+        Author author = new() { Name = authorDto.Name };
+
+        context.Authors.Add(author);
+
+        await context.SaveChangesAsync();
+
+        AuthorDto resultDto = new(author.Id, author.Name);
+
+        return TypedResults.Created($"/authors/{resultDto.Id}", resultDto);
+    }
+
     public static async Task<Ok<AuthorDto[]>> GetAllAuthors(LibraryContext context)
     {
         AuthorDto[] authors = await context.Authors.Select(a => new AuthorDto(a.Id, a.Name)).ToArrayAsync();
@@ -43,4 +58,35 @@ public static class WebApiVersion1
         }
     }
 
+    public static async Task<Results<NoContent, NotFound>> UpdateAuthor(int id, AuthorDto authorDto, LibraryContext context)
+    {
+        if (await context.Authors.FindAsync(id) is Author author)
+        {
+            author.Name = authorDto.Name;
+
+            await context.SaveChangesAsync();
+
+            return TypedResults.NoContent();
+        }
+        else
+        {
+            return TypedResults.NotFound();
+        }
+    }
+
+    public static async Task<Results<NoContent, NotFound>> DeleteAuthor(int id, LibraryContext context)
+    {
+        if (await context.Authors.FindAsync(id) is Author author)
+        {
+            context.Authors.Remove(author);
+
+            await context.SaveChangesAsync();
+
+            return TypedResults.NoContent();
+        }
+        else
+        {
+            return TypedResults.NotFound();
+        }
+    }
 }
