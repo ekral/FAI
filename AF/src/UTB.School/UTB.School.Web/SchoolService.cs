1b580b5831e5422c47964c77cddf6d3b4a6659f3
ekral <ekral@utb.cz>
2026-04-01 13:44:37 +0200

    add reseni

--- AF/src/UTB.School/UTB.School.Web/SchoolService.cs
+++ AF/src/UTB.School/UTB.School.Web/SchoolService.cs
@@ -29,6 +29,41 @@ namespace UTB.School.Web
             }
         }
 
+        public async Task<Result<StudentDto>> GetStudentAsync(int studentId)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync($"/students/{studentId}");
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    StudentDto? student = await response.Content.ReadFromJsonAsync<StudentDto>();
+
+                    if (student is null)
+                    {
+                        return Result<StudentDto>.Failure("Nepodařilo se načíst data studenta.");
+                    }
+
+                    return Result<StudentDto>.Success(student);
+                }
+
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return Result<StudentDto>.Failure("Požadovaný záznam neexistuje.");
+                }
+
+                return Result<StudentDto>.Failure($"Načtení studenta selhalo. Neočekávaný stav: {(int)response.StatusCode} ({response.StatusCode})");
+            }
+            catch (HttpRequestException)
+            {
+                return Result<StudentDto>.Failure("API není dostupné");
+            }
+            catch (TaskCanceledException)
+            {
+                return Result<StudentDto>.Failure("Timeout");
+            }
+        }
+
         public async Task<Result> CreateStudentAsync(StudentRequestDto requestDto)
         {
             try
@@ -41,6 +76,34 @@ namespace UTB.School.Web
                 }
 
                 return Result.Failure($"Vytvoření studenta selhalo. Neočekávaný stav: {(int)response.StatusCode} ({response.StatusCode})");
+            }
+            catch (HttpRequestException)
+            {
+                return Result.Failure("API není dostupné");
+            }
+            catch (TaskCanceledException)
+            {
+                return Result.Failure("Timeout");
+            }
+        }
+
+        public async Task<Result> UpdateStudentAsync(int studentId, StudentRequestDto requestDto)
+        {
+            try
+            {
+                var response = await httpClient.PutAsJsonAsync($"/students/{studentId}", requestDto);
+
+                if (response.StatusCode == HttpStatusCode.NoContent)
+                {
+                    return Result.Success();
+                }
+
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return Result.Failure("Požadovaný záznam neexistuje.");
+                }
+
+                return Result.Failure($"Uložení změn selhalo. Neočekávaný stav: {(int)response.StatusCode} ({response.StatusCode})");
             }
             catch (HttpRequestException)
             {
