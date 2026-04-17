44097e72d1b80a721b95668ba2a94794783a09f0
ekral <ekral@utb.cz>
2026-04-17 14:45:18 +0200

    Removed Update from Web

--- AF/src/UTB.School/UTB.School.WebSseJavascript/Program.cs
+++ AF/src/UTB.School/UTB.School.WebSseJavascript/Program.cs
@@ -1,8 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Povolí obsluhu statických souborů (wwwroot)
-app.UseDefaultFiles(); // automaticky hledá index.html
-app.UseStaticFiles();
+app.UseDefaultFiles(); // Automaticky hledá index.html
+app.UseStaticFiles();  // Povolí obsluhu statických souborů (wwwroot)
 
 app.Run();
