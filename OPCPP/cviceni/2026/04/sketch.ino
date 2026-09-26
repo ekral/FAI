03bceb604b6425c556e0ce1145ac460aa05e71ef
ekral <ekral@utb.cz>
2026-09-26 15:17:18 +0200

    add 04

---
+++ OPCPP/cviceni/2026/04/sketch.ino
@@ -0,0 +1,97 @@
+struct EdgeDetector
+{
+  bool candidateState;
+  unsigned long candidateSince;
+  bool lastStableState;
+
+  const bool targetState;
+  const unsigned long minStableDuration;
+
+  EdgeDetector(bool targetState, unsigned long minStableDuration)
+    : targetState(targetState), minStableDuration(minStableDuration)
+  {
+
+  }
+
+  void begin(bool state, unsigned long time)
+  {
+    candidateState = state;
+    candidateSince = time;
+    lastStableState = state;
+  }
+
+  bool detect(bool state, unsigned long time)
+  {
+    if(state != candidateState)
+    {
+      candidateState = state;
+      candidateSince = time;
+    }
+    else
+    {
+      unsigned long duration = time - candidateSince;
+
+      if(duration > minStableDuration)
+      {
+        if(candidateState != lastStableState)
+        {
+          lastStableState = candidateState;
+
+          if(candidateState == targetState)
+          {
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+};
+
+struct Led
+{
+  const int pin;
+  bool state;
+
+  Led(int pin) : pin(pin)
+  {
+  }
+
+  void writeState()
+  {
+    digitalWrite(pin, state);
+  }
+
+  void begin(bool state)
+  {
+    this->state = state;
+
+    writeState();
+  }
+
+  void toggle()
+  {
+    state = !state;
+
+    writeState();
+  }
+};
+
+EdgeDetector edge(HIGH, 30);
+Led led(13);
+
+void setup() 
+{
+  pinMode(12, INPUT_PULLUP);
+  edge.begin(digitalRead(12), millis());
+  led.begin(LOW);
+}
+
+void loop() 
+{
+  if(edge.detect(digitalRead(12), millis()))
+  {
+    led.toggle();
+  }
+}
